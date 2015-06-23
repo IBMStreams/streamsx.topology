9c11c10be13ff1c194a187df80546860a6fd907b
wcmarsha <wcmarsha@us.ibm.com>
2015-06-23 14:17:33 -0700

    Created third-party dependency test.

---
+++ test/java/src/com/ibm/streamsx/topology/test/api/ThirdParty.java
@@ -0,0 +1,58 @@
+package com.ibm.streamsx.topology.test.api;
+
+import com.ibm.streamsx.topology.TStream;
+import com.ibm.streamsx.topology.Topology;
+import com.ibm.streamsx.topology.function7.Function;
+import com.ibm.streamsx.topology.tester.Condition;
+import com.ibm.streamsx.topology.tester.Tester;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotSame;
+import static org.junit.Assert.assertSame;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ThirdParty {
+    @Test
+    public void includeThirdPartyJar() throws Exception {
+        final Topology topology = new Topology("BasicStream");
+        topology.addThirdPartyDependency("resources/ThirdPartyResource.jar");
+        TStream<String> source = topology.strings("1", "2", "3");
+        TStream<String> thirdPartyOutput = source.transform(new Function<String, String>(){
+            @Override
+            public String apply(String v) {            
+                return ThirdPartyResource.thirdPartyStaticMethod() + v;
+            }
+        }, String.class);
+        
+        Tester tester = topology.getTester();
+        Condition expectedCount = tester.tupleCount(thirdPartyOutput, 3);
+        Condition regionCount = tester.stringContents(thirdPartyOutput, 
+                "This was returned from a third-party method1",
+                "This was returned from a third-party method2",
+                "This was returned from a third-party method3");
+    }
+    
+    @Test
+    public void includeThirdPartyClass() throws Exception {
+        final Topology topology = new Topology("BasicStream");
+        topology.addThirdPartyDependency(ThirdPartyResource.class);
+        TStream<String> source = topology.strings("1", "2", "3");
+        TStream<String> thirdPartyOutput = source.transform(new Function<String, String>(){
+            private ThirdPartyResource tpr = new ThirdPartyResource("This string was set.");
+            
+            @Override
+            public String apply(String v) {            
+                return ThirdPartyResource.thirdPartyMethod() + v;
+            }
+        }, String.class);
+        
+        Tester tester = topology.getTester();
+        Condition expectedCount = tester.tupleCount(thirdPartyOutput, 3);
+        Condition regionCount = tester.stringContents(thirdPartyOutput, 
+                "This string was set.1",
+                "This string was set.2",
+                "This string was set.3");
+    }
+}
