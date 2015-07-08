a12e037b70b05813e1160800b3f821430a3d00e4
wcmarsha <wcmarsha@us.ibm.com>
2015-07-07 19:21:28 -0700

    Added documentation.

--- java/src/com/ibm/streamsx/topology/TStream.java
+++ java/src/com/ibm/streamsx/topology/TStream.java
@@ -509,22 +509,30 @@ public interface TStream<T> extends TopologyElement {
     TStream<T> sample(double fraction);
     
     /**
-     * Add docs
-     * @return isolate tstream
+     * Return a TStream that runs in a separate process from the parent. The
+     * children of the returned TStream will also run in another process. If a
+     * child of the returned TStream is combined with another TStream through a 
+     * union(), both TStreams will run in another process, including their ancestors
+     * and children up until other isolate() boundaries.
+     * <br><br>
+     * In other words, for the following Topology:
+     * <pre><code>
+     *                    |--.isolate()---transform2--|
+     * ------transform1---|                           |--.isolate()--filter---
+     *                    |--.isolate()---transform3--|
+     * </code></pre>
+     * 
+     * Three separate processes will be created -- one process will contain
+     * transform1, another will contain both transform2 and transform3, and the
+     * last will contain filter.
+     * 
+     * <br><br>
+     * Merging an isolated stream with its parent stream will throw an exception.
+     * @return A TStream whose children will run in a separate process from 
+     * their parents.
      */
     TStream<T> isolate();
-    
-    /**
-     * Start low latency region
-     * @return Low latency tstream
-     */
-    TStream<T> lowLatency();
-    
-    /**
-     * end low latency region
-     * @return Non-low latency tstream.
-     */
-    TStream<T> endLowLatency();
+
 
     /**
      * Throttle a stream by ensuring any tuple is submitted with least
