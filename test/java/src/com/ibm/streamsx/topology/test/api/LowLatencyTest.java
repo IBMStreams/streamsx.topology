8aa89159ae1e55591775d460dcf44cc414ac2ae2
wcmarsha <wcmarsha@us.ibm.com>
2015-07-13 19:51:39 -0700

    Added threadedPortTest to LowLatencyTest.java to ensure proper funcitoning of threaed ports in low latency regions.

--- test/java/src/com/ibm/streamsx/topology/test/api/LowLatencyTest.java
+++ test/java/src/com/ibm/streamsx/topology/test/api/LowLatencyTest.java
@@ -7,12 +7,14 @@ import java.math.BigInteger;
 
 import org.junit.Test;
 
+import com.ibm.json.java.JSONArray;
+import com.ibm.json.java.JSONObject;
 import com.ibm.streamsx.topology.TStream;
 import com.ibm.streamsx.topology.Topology;
 import com.ibm.streamsx.topology.context.StreamsContext;
 import com.ibm.streamsx.topology.context.StreamsContextFactory;
 import com.ibm.streamsx.topology.function7.Function;
-
+import com.ibm.streamsx.topology.generator.spl.SPLGenerator;
 import com.ibm.streams.operator.PERuntime;
 import com.ibm.streams.operator.Tuple;
 
@@ -49,6 +51,38 @@ public class LowLatencyTest {
         
         StreamsContextFactory.getStreamsContext(StreamsContext.Type.TOOLKIT).submit(topology).get();
     }
+    
+    @Test
+    public void threadedPortTest() throws Exception{
+        assumeTrue(SC_OK);
+        Topology topology = new Topology("lowLatencyTest");
+
+        // Construct topology
+        TStream<String> ss = topology.strings("hello").lowLatency();
+        TStream<String> ss1 = ss.transform(getPEId(), String.class);
+        TStream<String> ss2 = ss1.transform(getPEId(), String.class).endLowLatency();
+        
+        SPLGenerator generator = new SPLGenerator();
+        JSONObject graph = topology.builder().complete();
+        generator.generateSPL(graph);
+        
+        JSONArray ops = (JSONArray)graph.get("operators");
+        for(Object opObj : ops){
+            JSONObject op = (JSONObject)opObj;
+            String lowLatencyTag = (String) op.get("lowLatencyTag");
+            String kind = (String)op.get("kind");
+            JSONObject queue = (JSONObject) op.get("queue");
+            if(queue != null && (lowLatencyTag!=null || lowLatencyTag.equals(""))){
+                throw new IllegalStateException("Operator has threaded port when it shouldn't.");
+            }
+            if(queue != null 
+                    && kind.equals("com.ibm.streamsx.topology.functional.java::FunctionTransform")){
+                throw new IllegalStateException("Transform operator expecting threaded port; none found.");
+            }
+        }
+    }
+    
+
     
     @SuppressWarnings("serial")
     private static Function<String, String> getPEId(){
