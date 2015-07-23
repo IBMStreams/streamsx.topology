a746f4eff19879aab3870ed76c4c4afeacd147e5
Dale LaBossiere <dlaboss@us.ibm.com>
2015-07-23 13:13:46 -0400

    Address review comment.

--- java/src/com/ibm/streamsx/topology/TSink.java
+++ java/src/com/ibm/streamsx/topology/TSink.java
@@ -7,7 +7,7 @@ package com.ibm.streamsx.topology;
 import com.ibm.streamsx.topology.builder.BOperatorInvocation;
 
 /**
- * A {@link TopologyElement} handle to a {@code sink} operation.
+ * A handle to the {@code sink} of a {@code TStream}.
  * 
  * @see TStream#sink(com.ibm.streamsx.topology.function.Consumer)
  */
