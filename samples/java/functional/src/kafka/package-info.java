0ba909213c57d20348da1c20bbceef3a8d380641
Dale LaBossiere <dlaboss@us.ibm.com>
2015-08-17 13:17:54 -0400

    Add a Kafka sample

---
+++ samples/java/functional/src/kafka/package-info.java
@@ -0,0 +1,9 @@
+/*
+# Licensed Materials - Property of IBM
+# Copyright IBM Corp. 2015  
+ */
+/**
+ * A sample demonstrating integration with the Apache Kafka messaging system
+ * <a href="http://kafka.apache.org">http://kafka.apache.org</a>.
+ */
+package kafka;
