ff55efa2bb028efee4ce86bf2d3de0efa39878b2
henrychi <henrychi@us.ibm.com>
2016-04-09 06:58:19 -0700

    updates to spldoc and sample

---
+++ samples/python/topology/simple/transform_sample.py
@@ -0,0 +1,14 @@
+from streamsx.topology.topology import Topology
+import streamsx.topology.context
+import transform_sample_functions;
+
+def main():
+    topo = Topology("transform_sample")
+    source = topo.source(transform_sample_functions.int_strings_transform)
+    i1 = source.transform(transform_sample_functions.string_to_int)
+    i2 = i1.transform(transform_sample_functions.AddNum(17))
+    i2.sink(print)
+    streamsx.topology.context.submit("STANDALONE", topo.graph)
+
+if __name__ == '__main__':
+    main()
