ce29785e10f0d405847bea14964cb9f744de6896
henrychi <henrychi@us.ibm.com>
2016-04-11 23:23:29 -0700

    fixes #328

--- samples/python/topology/simple/transform_sample_functions.py
+++ samples/python/topology/simple/transform_sample_functions.py
@@ -1,11 +1,33 @@
 def int_strings_transform():
+    """
+    Returns an iterable of strings
+    """
     return ["325", "457", "9325"]
     
 def string_to_int(t):
+    """
+    Converts a string to integer
+    Args:
+        t (string): tuple
+    Returns:
+        int        
+    """
     return int(t)
 
 class AddNum:
+    """
+    A callable class that performs addition
+    Args:
+        increment (int): number to add
+    """
     def __init__(self, increment):
         self.increment = increment  
     def __call__(self, tuple):
+        """
+        Performs addition on the input value
+        Args:
+            tuple (int): tuple
+        Returns:
+            int
+        """
         return tuple + self.increment
