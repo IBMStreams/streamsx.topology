56158fa4a7cf3bf7d5b63480edb9858f91b3096d
Daniel J. Debrunner <debrunne@us.ibm.com>
2016-04-13 16:06:23 -0700

    Initial tweepy sample

---
+++ samples/python/topology/tweepy/app.py
@@ -0,0 +1,30 @@
+from streamsx.topology.topology import *
+import streamsx.topology.context
+import sys
+import tweets
+
+#
+# Continually stream tweets that contain
+# the terms passed on the command line.
+#
+# python3 app.py Food GlutenFree
+#
+def main():
+  terms = sys.argv[1:]
+  topo = Topology("TweetsUsingTweepy")
+
+  # Event based source stream
+  # Each tuple is a dictionary containing
+  # the full tweet (converted from JSON)
+  ts = topo.source(tweets.tweets(terms))
+  
+  # get the text of the tweet
+  ts = ts.transform(tweets.text)
+
+  # just print it
+  ts.print()
+
+  streamsx.topology.context.submit("DISTRIBUTED", topo.graph)
+
+if __name__ == '__main__':
+    main()
