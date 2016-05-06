8aae9fd3a48bde018514f7563cf1a789cce88248
Daniel J. Debrunner <debrunne@us.ibm.com>
2016-05-06 10:48:42 -0700

    Always set library and include path from current python3

--- com.ibm.streamsx.topology/com.ibm.streamsx.topology.functional.python/pyversion.sh
+++ com.ibm.streamsx.topology/com.ibm.streamsx.topology.functional.python/pyversion.sh
@@ -2,10 +2,11 @@
 action=$1
 if [ $action = "lib" ]
 then
+    # python3-config --libs
     :
 elif [ $action = "libPath" ]
 then
-    :
+    echo `python3-config --prefix`/lib
 elif [ $action = "includePath" ]
 then
     python3-config --includes
