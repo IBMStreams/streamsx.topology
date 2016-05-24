68ec81c6cda38cc4ea5499d57daee9c9f6fc32c4
Daniel J. Debrunner <debrunne@us.ibm.com>
2016-05-24 09:16:26 -0700

    Documentation improvements

--- com.ibm.streamsx.topology/opt/python/packages/streamsx/topology/schema.py
+++ com.ibm.streamsx.topology/opt/python/packages/streamsx/topology/schema.py
@@ -1,7 +1,7 @@
 import enum
 
 class StreamSchema(object) :
-    """SPL stream schema"""
+    """SPL stream schema."""
 
     def __init__(self, schema):
         self.__schema=schema.strip()
@@ -15,8 +15,10 @@ class StreamSchema(object) :
         _splj["value"] = self.schema()
         return _splj
 
-    # Extend a schema by another
     def extend(self, schema):
+        """
+        Extend a schema by another
+        """
         base = self.schema()
         extends = schema.schema()
         new_schema = base[:-1] + ',' + extends[6:]
@@ -26,7 +28,14 @@ class StreamSchema(object) :
 
 @enum.unique
 class CommonSchema(enum.Enum):
-    """Common stream schemas for interoperability"""
+    """
+    Common stream schemas for interoperability within Streams applications.
+    
+    Python - Stream constains Python objects
+    Json - Stream contains JSON objects. Streams with schema Json can be published and subscribed between Streams applications implemented in different languages.
+    String - Stream contains strings. Streams with schema String can be published and subscribed between Streams applications implemented in different languages.
+    Binary - Stream contains binary tuples. NOT YET SUPPORTED IN Python.
+    """
     Python = StreamSchema("tuple<blob __spl_po>")
     Json = StreamSchema("tuple<rstring jsonString>")
     String = StreamSchema("tuple<rstring string>")
