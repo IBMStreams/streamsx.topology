924738226ae7cce3eed872d443028e9204ff155f
Daniel J. Debrunner <debrunne@us.ibm.com>
2016-06-13 14:11:48 -0700

    Have a single version of pyversion.sh

--- com.ibm.streamsx.topology/opt/python/templates/common/pyversion.sh
+++ com.ibm.streamsx.topology/opt/python/templates/common/pyversion.sh
@@ -2,8 +2,7 @@
 action=$1
 if [ $action = "lib" ]
 then
-    # python3-config --libs
-    :
+    python3-config --libs | sed -e 's/^-l//;s/ -l/ /g;s/ \+/\n/g'
 elif [ $action = "libPath" ]
 then
     echo `python3-config --prefix`/lib
