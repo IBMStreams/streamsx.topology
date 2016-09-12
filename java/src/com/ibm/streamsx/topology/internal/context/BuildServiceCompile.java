e6687ce97824066145ae96868c80a5cf86f48880
wmarshall484 <wmarshall484@gmail.com>
2016-09-12 14:38:58 -0400

    Removed uneccessary file object.

--- java/src/com/ibm/streamsx/topology/internal/context/BuildServiceCompile.java
+++ java/src/com/ibm/streamsx/topology/internal/context/BuildServiceCompile.java
@@ -4,13 +4,11 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.nio.file.Path;
 import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 
 import org.apache.http.HttpEntity;
-import org.apache.http.HttpStatus;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.CloseableHttpResponse;
@@ -22,11 +20,7 @@ import org.apache.http.entity.mime.content.FileBody;
 import org.apache.http.entity.mime.content.StringBody;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.util.EntityUtils;
-
-import com.ibm.json.java.JSON;
 import com.ibm.json.java.JSONObject;
-import com.ibm.json.java.OrderedJSONObject;
 import com.ibm.streamsx.topology.Topology;
 import com.ibm.streamsx.topology.internal.process.CompletedFuture;
 
@@ -60,15 +54,13 @@ public class BuildServiceCompile extends ZippedToolkitStreamsContext {
 	private File doRemoteCompile(String namespace, String mainComposite, 
 			File zippedArchive) throws ClientProtocolException, IOException {
 		String bundleName = namespace + "." + mainComposite + ".sab";
-		String archiveName = zippedArchive.getName();
 		HttpClient client = HttpClientBuilder.create().build();
 		
-		File archiveFile = new File(archiveName);
 		HttpPost postJobWithConfig = new HttpPost(getBuildFarmURL());
         postJobWithConfig.addHeader("accept", ContentType.APPLICATION_JSON.getMimeType());
 
-        FileBody bundleBody = new FileBody(archiveFile, 
-        		ContentType.APPLICATION_OCTET_STREAM, archiveName);
+        FileBody bundleBody = new FileBody(zippedArchive, 
+        		ContentType.APPLICATION_OCTET_STREAM, zippedArchive.getName());
         
         StringBody configBody = new StringBody(createBuildConfig(namespace, mainComposite),
         		ContentType.TEXT_PLAIN);
