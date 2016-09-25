9c9c492c29b6c2814871fd0696b2827dbfafc001
Daniel J. Debrunner <debrunne@us.ibm.com>
2016-09-25 16:59:03 -0700

    Consolidate codegen perl subroutines into single shared file

--- com.ibm.streamsx.topology/opt/python/codegen/splpy.pm
+++ com.ibm.streamsx.topology/opt/python/codegen/splpy.pm
@@ -228,4 +228,95 @@ sub convertToPythonValueAsTuple {
   return $get . $assign ;
 }
 
+# Determine which style of argument is being
+# used. These match the SPL types in
+# com.ibm.streamsx.topology/types.spl
+#
+# SPL TYPE - style - comment
+# blob __spl_po - python - pickled Python object
+# rstring string - string - SPL rstring
+# rstring jsonString - json - JSON as SPL rstring
+# xml document - xml - XML document
+# blob binary - binary - Binary data
+#
+# tuple<...> - dict - Any SPL tuple type apart from above
+#
+# Not all are supported yet.
+# 
+
+sub splpy_tuplestyle{
+
+ my ($port) = @_;
+
+ my $attr =  $port->getAttributeAt(0);
+ my $attrtype = $attr->getSPLType();
+ my $attrname = $attr->getName();
+ my $pystyle = 'unk';
+ my $numattrs = $port->getNumberOfAttributes();
+
+ if (($numattrs == 1) && SPL::CodeGen::Type::isBlob($attrtype) && ($attrname eq '__spl_po')) {
+    $pystyle = 'pickle';
+ } elsif (($numattrs == 1) && SPL::CodeGen::Type::isRString($attrtype) && ($attrname eq 'string')) {
+    $pystyle = 'string';
+ } elsif (($numattrs == 1) && SPL::CodeGen::Type::isRString($attrtype) && ($attrname eq 'jsonString')) {
+    $pystyle = 'json';
+ } elsif (($numattrs == 1) && SPL::CodeGen::Type::isBlob($attrtype) && ($attrname eq 'binary')) {
+    $pystyle = 'binary';
+    SPL::CodeGen::errorln("Blob schema is not currently supported for Python."); 
+ } elsif (($numattrs == 1) && SPL::CodeGen::Type::isXml($attrtype) && ($attrname eq 'document')) {
+    $pystyle = 'xml';
+    SPL::CodeGen::errorln("XML schema is not currently supported for Python."); 
+ } else {
+    $pystyle = 'dict';
+ }
+
+ return $pystyle;
+}
+
+# Given a style return a string containing
+# the C++ code to get the value
+# from an input tuple ip, that will
+# be converted to Python and passed to the function.
+#
+# Must setup a C++ variable called 'value' that
+# represents the value to be passed into the Python function
+#
+sub splpy_inputtuple2value{
+ my ($pystyle) = @_;
+ if ($pystyle eq 'pickle') {
+  return 'SPL::blob const & value = ip.get___spl_po();';
+ }
+
+ if ($pystyle eq 'string') {
+  return 'SPL::rstring const & value = ip.get_string();';
+ }
+ 
+ if ($pystyle eq 'json') {
+  return 'SPL::rstring const & value = ip.get_jsonString();';
+ }
+
+ if ($pystyle eq 'dict') {
+  # nothing done here for dict style 
+ }
+}
+
+sub convertAndAddToPythonDictionaryObject {
+  my $ituple = $_[0];
+  my $i = $_[1];
+  my $type = $_[2];
+  my $name = $_[3];
+
+  my $get = '{ PyObject * pyValue = ';
+  $get = $get . convertToPythonValue($ituple, $type, $name);
+
+  $getkey = '{ PyObject * pyDictKey = PyUnicode_DecodeUTF8((const char*)  "' . $name . '", ((int)(sizeof("' . $name . '")))-1 , NULL);'."\n";
+
+# Note PyDict_SetItem does not steal the references to the key and value
+  my $setdict =  "  PyDict_SetItem(pyDict, pyDictKey, pyValue);\n";
+  $setdict =  $setdict . "  Py_DECREF(pyDictKey);\n";
+  $setdict =  $setdict . "  Py_DECREF(pyValue);}}\n";
+
+  return $get . $getkey . $setdict ;
+}
+
 1;
