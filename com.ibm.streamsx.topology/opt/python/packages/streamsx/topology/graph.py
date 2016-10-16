3313b2b4d7d7304830ba4dc5ec58e9703ffbc647
Daniel J. Debrunner <debrunne@us.ibm.com>
2016-10-16 14:52:43 -0700

    Support lambda functions

--- com.ibm.streamsx.topology/opt/python/packages/streamsx/topology/graph.py
+++ com.ibm.streamsx.topology/opt/python/packages/streamsx/topology/graph.py
@@ -1,10 +1,17 @@
 # Licensed Materials - Property of IBM
-# Copyright IBM Corp. 2015
+# Copyright IBM Corp. 2015,2016
 
 import uuid
 import json
 import inspect
 import pickle
+
+try:
+    import dill
+except ImportError:
+    dill = pickle
+
+import types
 import base64
 import streamsx.topology.dependency
 import streamsx.topology.functions
@@ -201,6 +208,10 @@ class SPLInvocation(object):
             return None
         if not hasattr(function, "__call__"):
             raise "argument to _addOperatorFunction is not callable"
+
+        # Wrap a lambda as a callable class instance
+        if isinstance(function, types.LambdaType) and function.__name__ == "<lambda>" :
+            function = streamsx.topology.functions._Callable(function)
                  
         if inspect.isroutine(function):
             # callable is a function
@@ -208,8 +219,8 @@ class SPLInvocation(object):
         else:
             # callable is a callable class instance
             self.params["pyName"] = function.__class__.__name__
-            # pickle format is binary; base64 encode so it is json serializable 
-            self.params["pyCallable"] = base64.b64encode(pickle.dumps(function)).decode("ascii")
+            # dill format is binary; base64 encode so it is json serializable 
+            self.params["pyCallable"] = base64.b64encode(dill.dumps(function)).decode("ascii")
 
         # note: functions in the __main__ module cannot be used as input to operations 
         # function.__module__ will be '__main__', so C++ operators cannot import the module
