930f3c0aed77c524f405033147fd87ba0dcf1dee
streamsadmin <streamsadmin@localhost.localdomain>
2016-11-10 02:03:00 -0500

    Updated doc. Correctly initialized logger.

--- com.ibm.streamsx.topology/opt/python/packages/streamsx/topology/context.py
+++ com.ibm.streamsx.topology/opt/python/packages/streamsx/topology/context.py
@@ -11,9 +11,8 @@ except (ImportError, NameError):
 # Licensed Materials - Property of IBM
 # Copyright IBM Corp. 2015
 
+from streamsx.topology import logging_utils
 import logging
-
-
 import tempfile
 import os
 import os.path
@@ -24,8 +23,8 @@ import sys
 
 from platform import python_version
 
+logging_utils.initialize_logging()
 logger = logging.getLogger('streamsx.topology.py_submit')
-
 
 #
 # Submission of a python graph using the Java Application API
@@ -40,13 +39,20 @@ def submit(ctxtype, app_topology, config=None, username=None, password=None, res
     Args:
         ctxtype (string): context type.  Values include:
         * DISTRIBUTED - the topology is submitted to a Streams instance.
-          The bundle is submitted using `streamtool` which must be setup to submit without requiring authentication input
+          The bundle is submitted using `streamtool` which must be setup to submit without requiring authentication
+          input. Additionally, a username and password may optionally be provided to enable retrieving data from remote
+          views.
         * STANDALONE - the topology is executed directly as an Streams standalone application.
           The standalone execution is spawned as a separate process
         * BUNDLE - execution of the topology produces an SPL application bundle
           (.sab file) that can be submitted to an IBM Streams instance as a distributed application.
         * JUPYTER - the topology is run in standalone mode, and context.submit returns a stdout streams of bytes which 
           can be read from to visualize the output of the application.
+        * BUILD_ARCHIVE - Creates a Bluemix-compatible build archive.
+          execution of the topology produces a build archive, which can be submitted to a streaming
+          analytics Bluemix remote build service.
+        * REMOTE_BUILD_AND_SUBMIT - the application is submitted to and built on a Bluemix streaming analytics service.
+          It is then submitted as a job.
         app_topology: a Topology object or Topology.graph object
         
     Returns:
@@ -101,7 +107,7 @@ class _BaseSubmitter:
             cp = cp + ':' + os.path.join(streams_install, "lib", "com.ibm.streams.operator.samples.jar")
 
         args = [jvm, '-classpath', cp, submit_class, self.ctxtype, self.fn]
-        logger.trace("Generating SPL and submitting application.")
+        logger.info("Generating SPL and submitting application.")
         process = subprocess.Popen(args, stdin=None, stdout=subprocess.PIPE, stderr=subprocess.PIPE, bufsize=0)
         try:
             stderr_thread = threading.Thread(target=_print_process_stderr, args=([process, self.fn]))
@@ -314,10 +320,6 @@ class _SubmitContextFactory:
         if self.config is None:
             self.config = {}
 
-        # deserialize vcap config before passing it off to Java
-        if 'topology.service.vcap' in self.config:
-            self.config['topology.service.vcap'] = json.loads(self.config['topology.service.vcap'])
-
     def get_submit_context(self, ctxtype):
 
         # If there is no streams install present, currently only REMOTE_BUILD_AND_SUBMIT, TOOLKIT, and BUILD_ARCHIVE
@@ -327,15 +329,15 @@ class _SubmitContextFactory:
             if not (ctxtype == 'REMOTE_BUILD_AND_SUBMIT' or ctxtype == 'TOOLKIT' or ctxtype == 'BUILD_ARCHIVE'):
                 raise UnsupportedContextException(ctxtype + " must be submitted when a streams install is present.")
 
+
+        if ctxtype == 'JUPYTER':
+            return _JupyterSubmitter(ctxtype, self.config, self.app_topology)
+        elif ctxtype == 'REMOTE_BUILD_AND_SUBMIT':
+            return _RemoteBuildSubmitter(ctxtype, self.config, self.app_topology)
+        elif ctxtype == 'DISTRIBUTED':
+            return _DistributedSubmitter(ctxtype, self.config, self.app_topology, self.username, self.password)
         else:
-            if ctxtype == 'JUPYTER':
-                return _JupyterSubmitter(ctxtype, self.config, self.app_topology)
-            elif ctxtype == 'REMOTE_BUILD_AND_SUBMIT':
-                return _RemoteBuildSubmitter(ctxtype, self.config, self.app_topology)
-            elif ctxtype == 'DISTRIBUTED':
-                return _DistributedSubmitter(ctxtype, self.config, self.app_topology, self.username, self.password)
-            else:
-                return _BaseSubmitter(ctxtype, self.config, self.app_topology)
+            return _BaseSubmitter(ctxtype, self.config, self.app_topology)
 
 
 # Used to delete the JSON file after it is no longer needed.
