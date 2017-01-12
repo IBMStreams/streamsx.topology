b9c6645bd704a2101c9023d5bccae5197f21a18c
Daniel J. Debrunner <debrunne@us.ibm.com>
2017-01-12 12:27:53 -0800

    More symbols loaded dynamically

--- com.ibm.streamsx.topology/opt/python/include/splpy_sym.h
+++ com.ibm.streamsx.topology/opt/python/include/splpy_sym.h
@@ -20,8 +20,9 @@
 #ifndef __SPL__SPLPY_SYM_H
 #define __SPL__SPLPY_SYM_H
 
-#include "Python.h"
+#pragma weak _Py_NoneStruct
 
+#include "Python.h"
 
 /**
  * For a Python C API function symbol PyXXX we create
@@ -49,6 +50,11 @@
  * Generic typedefs potentially shared by more than one function.
  */
 typedef PyObject * (*__splpy_p_p_fp)(PyObject *);
+typedef PyObject * (*__splpy_p_pp_fp)(PyObject *, PyObject *);
+typedef PyObject * (*__splpy_p_ppp_fp)(PyObject *, PyObject *, PyObject *);
+typedef PyObject * (*__splpy_p_s_fp)(Py_ssize_t);
+typedef char * (*__splpy_c_p_fp)(PyObject *);
+typedef int (*__splpy_i_p_fp)(PyObject *);
 
 /*
  * GIL State locks
@@ -79,6 +85,7 @@ typedef PyObject* (*__splpy_udu_fp)(const char *, Py_ssize_t, const char *);
 extern "C" {
   static __splpy_p_p_fp __spl_fp_PyObject_Str;
   static __splpy_udu_fp __spl_fp_PyUnicode_DecodeUTF8;
+  static __splpy_c_p_fp __spl_fp_PyBytes_AsString;
 
   static PyObject * __spl_fi_PyObject_Str(PyObject *v) {
      return __spl_fp_PyObject_Str(v);
@@ -86,9 +93,13 @@ extern "C" {
   static PyObject * __spl_fi_PyUnicode_DecodeUTF8(const char *s, Py_ssize_t size, const char * errors) {
      return __spl_fp_PyUnicode_DecodeUTF8(s, size, errors);
   }
+  static char * __spl_fi_PyBytes_AsString(PyObject * o) {
+     return __spl_fp_PyBytes_AsString(o);
+  }
 }
 #pragma weak PyObject_Str = __spl_fi_PyObject_Str
 #pragma weak PyUnicode_DecodeUTF8 = __spl_fi_PyUnicode_DecodeUTF8
+#pragma weak PyBytes_AsString = __spl_fi_PyBytes_AsString
 
 /*
  * Loading modules, running code
@@ -100,6 +111,9 @@ typedef int (*__splpy_rssf_fp)(const char *, PyCompilerFlags *);
 extern "C" {
   static __splpy_ogas_fp __spl_fp_PyObject_GetAttrString;
   static __splpy_rssf_fp __spl_fp_PyRun_SimpleStringFlags;
+  static __splpy_p_ppp_fp __spl_fp_PyObject_Call;
+  static __splpy_p_pp_fp __spl_fp_PyObject_CallObject;
+  static __splpy_i_p_fp __spl_fp_PyCallable_Check;
 
   static PyObject * __spl_fi_PyObject_GetAttrString(PyObject *o, const char * attr_name) {
      return __spl_fp_PyObject_GetAttrString(o, attr_name);
@@ -107,9 +121,46 @@ extern "C" {
   static int __spl_fi_PyRun_SimpleStringFlags(const char * command, PyCompilerFlags *flags) {
      return __spl_fp_PyRun_SimpleStringFlags(command, flags);
   }
+  static PyObject * __spl_fi_PyObject_Call(PyObject *callable, PyObject * args, PyObject * kwargs) {
+     return __spl_fp_PyObject_Call(callable, args, kwargs);
+  }
+  static PyObject * __spl_fi_PyObject_CallObject(PyObject *callable, PyObject * args) {
+     return __spl_fp_PyObject_CallObject(callable, args);
+  }
+  static int __spl_fi_PyCallable_Check(PyObject *o) {
+     return __spl_fp_PyCallable_Check(o);
+  }
 }
 #pragma weak PyObject_GetAttrString = __spl_fi_PyObject_GetAttrString
 #pragma weak PyRun_SimpleStringFlags = __spl_fi_PyRun_SimpleStringFlags
+#pragma weak PyObject_Call = __spl_fi_PyObject_Call
+#pragma weak PyObject_CallObject = __spl_fi_PyObject_CallObject
+#pragma weak PyCallable_Check = __spl_fi_PyCallable_Check
+
+/*
+ * Container Objects
+ */
+extern "C" {
+  static __splpy_p_s_fp __spl_fp_PyTuple_New;
+
+  static PyObject * __spl_fi_PyTuple_New(Py_ssize_t size) {
+     return __spl_fp_PyTuple_New(size);
+  }
+}
+#pragma weak PyTuple_New = __spl_fi_PyTuple_New
+
+/*
+ * Err Objects
+ */
+typedef void (*__splpy_ef_fp)(PyObject **, PyObject **, PyObject **);
+extern "C" {
+  static __splpy_ef_fp __spl_fp_PyErr_Fetch;
+
+  static void __spl_fi_PyErr_Fetch(PyObject **t, PyObject **v, PyObject **tb) {
+     __spl_fp_PyErr_Fetch(t,v,tb);
+  }
+}
+#pragma weak PyErr_Fetch = __spl_fi_PyErr_Fetch
 
 #define __SPLFIX(_NAME, _TYPE) \
      __spl_fp_##_NAME = ( _TYPE ) dlsym(pydl, #_NAME )
@@ -127,9 +178,17 @@ class SplpySym {
 
      __SPLFIX(PyObject_Str, __splpy_p_p_fp);
      __SPLFIX(PyUnicode_DecodeUTF8, __splpy_udu_fp);
+     __SPLFIX(PyBytes_AsString, __splpy_c_p_fp);
 
      __SPLFIX(PyObject_GetAttrString, __splpy_ogas_fp);
      __SPLFIX(PyRun_SimpleStringFlags, __splpy_rssf_fp);
+     __SPLFIX(PyObject_Call, __splpy_p_ppp_fp);
+     __SPLFIX(PyObject_CallObject, __splpy_p_pp_fp);
+     __SPLFIX(PyCallable_Check, __splpy_i_p_fp);
+ 
+     __SPLFIX(PyTuple_New, __splpy_p_s_fp);
+
+     __SPLFIX(PyErr_Fetch, __splpy_ef_fp);
    }
 };
 
