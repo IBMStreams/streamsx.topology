55e8cf9e1efc22494502dfb07a298f38411bc0d3
Daniel J. Debrunner <debrunne@us.ibm.com>
2017-01-24 15:40:33 -0800

    Fix copyrights

--- com.ibm.streamsx.topology/opt/python/packages/streamsx/topology/context.py
+++ com.ibm.streamsx.topology/opt/python/packages/streamsx/topology/context.py
@@ -1,3 +1,5 @@
+# Licensed Materials - Property of IBM
+# Copyright IBM Corp. 2016,2017
 from __future__ import print_function
 from __future__ import unicode_literals
 from __future__ import division
