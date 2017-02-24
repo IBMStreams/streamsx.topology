c7691264c545854ca05e9c1195f4154d747043ce
joergboe <joergboe@de.ibm.com>
2017-02-24 14:25:49 +0100

    Add description for translation verification test script

--- test/python/spl/translation/autorun.sh
+++ test/python/spl/translation/autorun.sh
@@ -9,6 +9,9 @@ function usage () {
 	cat <<EOM
 
 This script makes some tests and demonstrated the output in all available languages
+The test script is made to support the translation verification test. It generates 3 log-messages in all available languages.
+These messages may be used to verify the translation.
+So it is currently nor suited for automated test
 
 	usage: $command [-h]
 
