6c71348ee26ac1a5ab4fb6ffdca7d2a2e8763392
Daniel J. Debrunner <debrunne@us.ibm.com>
2017-03-03 08:57:35 -0500

    Fix test2_pkg setup

--- test/python/topology/test2_pkg.py
+++ test/python/topology/test2_pkg.py
@@ -17,8 +17,6 @@ import streamsx.topology.context
 import test_package.test_subpackage.test_module
 import test2_pkg_helpers
 
-ctxtype = "STANDALONE"
-
 @unittest.skipIf(not test_vers.tester_supported() , "tester not supported")
 class TestPackages(unittest.TestCase):
   def setUp(self):
@@ -32,7 +30,7 @@ class TestPackages(unittest.TestCase):
       hwf = hw.filter(test_package.test_subpackage.test_module.filter)
       tester = Tester(topo)
       tester.contents(hwf, ["Hello"])
-      tester.test(ctxtype)
+      tester.test(self.test_ctxtype, self.test_config)
 
   # indirect import is included
   def test_TopologyIndirectPackage(self):
@@ -41,7 +39,7 @@ class TestPackages(unittest.TestCase):
       hwf = hw.transform(test2_pkg_helpers.imported_package)
       tester = Tester(topo)
       tester.contents(hwf, ["HelloIP"])
-      tester.test(ctxtype)
+      tester.test(self.test_ctxtype, self.test_config)
 
   # Now test that if we exclude the package it is not found
   def test_TopologyExcludePackage(self):
@@ -51,12 +49,12 @@ class TestPackages(unittest.TestCase):
       hwf = hw.transform(test2_pkg_helpers.missing_package)
       tester = Tester(topo)
       tester.contents(hwf, ["HelloMP", "World!MP"])
-      tester.test(ctxtype)
+      tester.test(self.test_ctxtype, self.test_config)
 
 @unittest.skipIf(not test_vers.tester_supported() , "Tester not supported")
 class TestDistributedPackages(TestPackages):
   def setUp(self):
-      Tester.setup_standalone(self)
+      Tester.setup_distributed(self)
 
 @unittest.skipIf(not test_vers.tester_supported() , "Tester not supported")
 class TestBluemixPackages(TestPackages):
