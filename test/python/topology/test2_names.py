5d024deca433c6b9d135bac848e7a2a79cd35e09
Daniel J. Debrunner <debrunne@us.ibm.com>
2017-03-03 11:30:53 -0500

    add test for a unicode name/namespace

--- test/python/topology/test2_names.py
+++ test/python/topology/test2_names.py
@@ -1,5 +1,6 @@
+# coding=utf-8
 # Licensed Materials - Property of IBM
-# Copyright IBM Corp. 2016
+# Copyright IBM Corp. 2017
 import unittest
 import sys
 import itertools
@@ -19,6 +20,20 @@ class TestNames(unittest.TestCase):
      """ Test default topo names from a notebook
      """
      topo = Topology(name='<module>', namespace='<ipython-input-1-e300f4c6abce>')
+     hw = topo.source(["Hello", "Tester"])
+     tester = Tester(topo)
+     tester.contents(hw, ["Hello", "Tester"])
+     tester.test(self.test_ctxtype, self.test_config)
+
+  def test_UnicodeTopoNames(self):
+     """ Test unicode topo names
+     """
+     n = '你好'
+     ns = '世界'
+     topo = Topology(name=n, namespace=ns)
+
+     self.assertEqual(n, topo.name)
+     self.assertEqual(ns, topo.namespace)
      hw = topo.source(["Hello", "Tester"])
      tester = Tester(topo)
      tester.contents(hw, ["Hello", "Tester"])
