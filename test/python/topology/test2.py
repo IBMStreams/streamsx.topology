550ad36f87fa9f57bc1f82ed50554f53e65d3878
Kendrick Wong <kendrick@ca.ibm.com>
2017-03-07 16:09:38 -0500

    Change bundleRoot-->bundlePath. Adds 'bundlePath' to submission result only if keepArtifacts is True or BUNDLE submission context is used. Fixes IBMStreams/streamsx.topology#775

--- test/python/topology/test2.py
+++ test/python/topology/test2.py
@@ -3,6 +3,7 @@
 import unittest
 import sys
 import itertools
+import os
 
 from streamsx.topology.topology import *
 from streamsx.topology.tester import Tester
@@ -11,6 +12,18 @@ import test_vers
 
 def s4():
     return ['one', 'two', 'three', 'four']
+
+
+@unittest.skipIf(not test_vers.tester_supported() , "Tester not supported")
+class TestBundleMethodsNew(unittest.TestCase):
+
+  def test_BundlePath(self):
+     topo = Topology("test_TopologySource")
+     hw = topo.source(["Hello", "Bundle"])
+     result = streamsx.topology.context.submit("BUNDLE", topo)
+     self.assertIn('bundlePath', result)
+     self.assertTrue(os.path.isfile(result['bundlePath']))
+
 
 @unittest.skipIf(not test_vers.tester_supported() , "Tester not supported")
 class TestTopologyMethodsNew(unittest.TestCase):
@@ -24,6 +37,13 @@ class TestTopologyMethodsNew(unittest.TestCase):
      tester = Tester(topo)
      tester.contents(hw, ["Hello", "Tester"])
      tester.test(self.test_ctxtype, self.test_config)
+     # bundlePath must exist if keepArtifacts is True
+     result = tester.result['submission_result']
+     if self.test_config.get('topology.keepArtifacts', False):
+         self.assertIn('bundlePath', result)
+         self.assertTrue(os.path.isfile(result['bundlePath']))
+     else:
+         self.assertNotIn('bundlePath', result)
 
   def test_TopologySourceFn(self):
      topo = Topology('test_TopologySourceFn')
@@ -31,7 +51,12 @@ class TestTopologyMethodsNew(unittest.TestCase):
      tester = Tester(topo)
      tester.contents(hw, s4())
      tester.tuple_count(hw, len(s4()))
+     self.test_config['topology.keepArtifacts'] = True
      tester.test(self.test_ctxtype, self.test_config)
+     # bundlePath must exist if keepArtifacts is True
+     result = tester.result['submission_result']
+     self.assertIn('bundlePath', result)
+     self.assertTrue(os.path.isfile(result['bundlePath']))
 
   def test_TopologySourceItertools(self):
      topo = Topology('test_TopologySourceItertools')
