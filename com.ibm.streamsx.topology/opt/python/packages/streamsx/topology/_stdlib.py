126a0145f9cd764dfffe950c4e0d82147ed62415
Daniel J. Debrunner <debrunne@us.ibm.com>
2017-03-16 18:20:41 -0400

    Allow streamsx modules to exist outside the main streamsx. Exclude standard library modules

---
+++ com.ibm.streamsx.topology/opt/python/packages/streamsx/topology/_stdlib.py
@@ -0,0 +1,109 @@
+#
+# Standard library module names:
+# https://docs.python.org/3/library/
+#
+_STD_LIB_MODULES = {
+
+    # 6. Text Processing Services',
+    'string',
+    're',
+    'difflib',
+    'textwrap',
+    'unicodedata',
+    'stringprep',
+    'readline',
+    'rlcompleter',
+
+    # 7. Binary Data Services'
+    'struct',
+    'codecs',
+
+    # 8. Data Types',
+    'datetime',
+    'calendar',
+    'collections',
+    'collections.abc',
+    'heapq',
+    'bisect',
+    'array',
+    'weakref',
+    'types',
+    'copy',
+    'pprint',
+    'reprlib',
+    'enum',
+
+    # 9. Numeric and Mathematical Modules
+    'numbers',
+    'math',
+    'cmath',
+    'decimal',
+    'fractions',
+    'random',
+    'statistics',
+
+    #10. Functional Programming Modules
+    'itertools',
+    'functools',
+    'operator',
+
+    # 11 File and directory access
+    'pathlib',
+    'os.path',
+    'fileinput',
+    'stat',
+    'filecmp',
+    'tempfile',
+    'glob',
+    'fnmatch',
+    'linecache',
+    'shutil',
+    'macpath',
+
+    # 12 Data Persistence
+    'pickle',
+    'copyreg',
+    'shelve',
+    'marshal',
+    'dbm',
+    'sqlite3',
+
+    # 13 Data Compression and Archiving
+    'zlib',
+    'gzip',
+    'bz2',
+    'lzma',
+    'zipfile',
+    'tarfile',
+
+    # 14. File Formats
+    'csv',
+    'configparser',
+    'netrc',
+    'xdrlib',
+    'plistlib',
+
+    # 15. Cryptographic Services
+
+    'hashlib',
+    'hmac',
+    'secrets',
+
+    # 16. Generic Operating System Services
+    'os',
+    'io',
+    'time',
+    'argparse',
+    'getopt',
+    'logging',
+    'logging.config',
+    'logging.handlers',
+    'getpass',
+    'curses',
+    'curses.textpad',
+    'curses.ascii',
+    'curses.panel',
+    'platform',
+    'errno',
+    'ctypes'
+}
