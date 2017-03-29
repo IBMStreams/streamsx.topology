02254ebcbf09204f1aa3e676d8ad4eb116e4e1fe
Daniel J. Debrunner <debrunne@us.ibm.com>
2017-03-29 18:12:07 -0400

    Add more instance/job calls

--- test/python/rest/primitives_caller.py
+++ test/python/rest/primitives_caller.py
@@ -15,8 +15,11 @@ def _fetch_from_instance(tc, instance):
     _check_non_empty_list(tc, instance.get_operators(), Operator)
     _check_non_empty_list(tc, instance.get_views(), View)
     _check_non_empty_list(tc, instance.get_operator_connections(), OperatorConnection)
+    _check_non_empty_list(tc, instance.get_resource_allocations(), ResourceAllocation)
+    _check_non_empty_list(tc, instance.get_active_services(), ActiveService)
 
     _check_list(tc, instance.get_exported_streams(), ExportedStream)
+    _check_list(tc, instance.get_imported_streams(), ImportedStream)
     _check_list(tc, instance.get_pe_connections(), PEConnection)
 
     tc.assertIsInstance(instance.get_domain(), Domain)
@@ -33,6 +36,7 @@ def _fetch_from_job(tc, job):
     _check_non_empty_list(tc, job.get_operators(), Operator)
     _check_non_empty_list(tc, job.get_views(), View)
     _check_non_empty_list(tc, job.get_operator_connections(), OperatorConnection)
+    _check_non_empty_list(tc, job.get_resource_allocations(), ResourceAllocation)
 
     _check_list(tc, job.get_pe_connections(), PEConnection)
 
