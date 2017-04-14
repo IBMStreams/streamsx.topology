8d9bd0aeb7dc688c0009b839628a0902347d4f70
Daniel J. Debrunner <debrunne@us.ibm.com>
2017-04-14 15:20:53 -0400

    Minimal travis build

--- test/python/travisci.sh
+++ test/python/travisci.sh
@@ -1,3 +1,12 @@
+cd $TRAVIS_BUILD_DIR/java
+export STREAMS_INSTALL=/dev/null
+ant compile.pure _jar
+
+exit 0
+
+# TBD - Work in progress
+unset STREAMS_INSTALL
+
+cd $TRAVIS_BUILD_DIR/test/python/topology
 export PYTHONPATH=$TRAVIS_BUILD_DIR/com.ibm.streamsx.topology/opt/python/packages
-cd test/python/topology
 python -u -m unittest test2.py
