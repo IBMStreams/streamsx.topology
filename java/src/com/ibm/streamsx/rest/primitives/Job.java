d782e9040b3c5765b3558eba63228fddde2e9bf7
Yvonne Chan <yvchan@ca.ibm.com>
2017-04-18 16:42:24 -0400

    initial rest api commit

---
+++ java/src/com/ibm/streamsx/rest/primitives/Job.java
@@ -0,0 +1,38 @@
+package com.ibm.streamsx.rest.primitives;
+
+public class Job
+{
+
+   public Job() {} ;
+
+   public String activeViews ;
+   public String adlFile ; 
+   public String applicationName ; 
+   public String applicationPath ; 
+   public String applicationScope ; 
+   public String applicationVersion ; 
+   public String checkpointPath ; 
+   public String dataPath ; 
+   public String domain ; 
+   public String health ; 
+   public String hosts ; 
+   public String id ; 
+   public String instance ; 
+   public String jobGroup ; 
+   public String name ; 
+   public String operatorConnections ; 
+   public String operators ; 
+   public String outputPath ; 
+   public String peConnections ; 
+   public String pes ; 
+   public String resourceAllocations ; 
+   public String resourceType ; 
+   public String restid ; 
+   public String self ; 
+   public String startedBy ; 
+   public String status ; 
+   public String submitParameters ; 
+   public long submitTime ; 
+   public String views ; 
+
+}
---
+++ java/src/com/ibm/streamsx/rest/primitives/Operator.java
@@ -0,0 +1,25 @@
+package com.ibm.streamsx.rest.primitives;
+
+public class Operator
+{
+
+   public Operator() {} ;
+
+   public String connections ; 
+   public String domain ; 
+   public String host ; 
+   public long indexWithinJob ; 
+   public String inputPorts ; 
+   public String instance ; 
+   public String job ; 
+   public String metrics ; 
+   public String name ; 
+   public String operatorKind ; 
+   public String outputPorts ; 
+   public String pe ; 
+   public String resourceAllocation ; 
+   public String resourceType ; 
+   public String restid ; 
+   public String self ; 
+
+}
