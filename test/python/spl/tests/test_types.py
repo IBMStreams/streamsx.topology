4fa50f75cd2bc06932f8392d02e1db2005268445
Daniel J. Debrunner <debrunne@us.ibm.com>
2017-04-18 13:48:30 -0400

    Initial blob support (not complete)

---
+++ test/python/spl/tests/test_types.py
@@ -0,0 +1,40 @@
+# Licensed Materials - Property of IBM
+# Copyright IBM Corp. 2016
+import unittest
+import sys
+import itertools
+
+from streamsx.topology.topology import *
+from streamsx.topology.tester import Tester
+from streamsx.topology import schema
+import streamsx.topology.context
+import streamsx.spl.op as op
+import streamsx.spl.toolkit
+
+
+class TestSPL(unittest.TestCase):
+    """ Test invocations of SPL operators from Python topology.
+    """
+    def setUp(self):
+        Tester.setup_standalone(self)
+
+    def test_blob_type(self):
+        topo = Topology()
+        streamsx.spl.toolkit.add_toolkit(topo, '/home/streamsadmin/topology/42test/streamsx.topology/test/python/spl/testtkpy')
+        data = ['Hello', 'Blob', 'Did', 'you', 'reset' ]
+        s = topo.source(data)
+        s = s.as_string()
+
+        toBlob = op.Map(
+            "com.ibm.streamsx.topology.pytest.pytypes::ToBlob",
+            s,
+            'tuple<blob b>')
+
+        bt = op.Map(
+            "com.ibm.streamsx.topology.pytest.pytypes::BlobTest",
+            toBlob.stream,
+            'tuple<rstring string>')
+         
+        tester = Tester(topo)
+        tester.contents(bt.stream, data)
+        tester.test(self.test_ctxtype, self.test_config)
---
+++ test/python/spl/testtkpy/opt/python/streams/test_types.py
@@ -0,0 +1,44 @@
+# Licensed Materials - Property of IBM
+# Copyright IBM Corp. 2016
+
+# Import the SPL decorators
+from streamsx.spl import spl
+
+#------------------------------------------------------------------
+# Test passing in SPL types functions
+#------------------------------------------------------------------
+
+# Defines the SPL namespace for any functions in this module
+# Multiple modules can map to the same namespace
+def splNamespace():
+    return "com.ibm.streamsx.topology.pytest.pytypes"
+
+@spl.map()
+def ToBlob(*s):
+    return (s[0].encode('utf-8'),)
+
+@spl.map()
+class BlobTest:
+    """
+    Expect blob tuples, need to verify that after
+    the call the previous value cannot be accessed.
+    """
+    def __init__(self):
+        self.last = list()
+
+    def __call__(self, *tuple):
+        v = tuple[0]
+        if not isinstance(v, memoryview):
+            return "Expected memory view is" + str(type(v))
+
+        if not v.readonly:
+            return "Expected readonly memory view"
+           
+        if self.last:
+            for b in self.last:
+                #TODO verify value has been released.
+                pass
+                
+        self.last.append(v)
+        return str(v, 'utf-8'),
+
