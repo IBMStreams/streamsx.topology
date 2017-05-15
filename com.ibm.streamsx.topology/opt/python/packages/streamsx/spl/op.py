5388b16e5063253582f12b978a17dd9a203b0173
Daniel J. Debrunner <debrunne@us.ibm.com>
2017-05-15 12:19:29 -0700

    Fix None in pydoc to be consistent

--- com.ibm.streamsx.topology/opt/python/packages/streamsx/spl/op.py
+++ com.ibm.streamsx.topology/opt/python/packages/streamsx/spl/op.py
@@ -122,15 +122,15 @@ class Invoke(exop.ExtensionOperator):
         topology(Topology): Topology that will invoke the operator.
         kind(str): SPL operator kind, e.g. ``spl.utility::Beacon``.
         inputs: Streams to connect to the operator. If not set or set to
-            ``None`` or an empty collection then the operator has no
+            `None` or an empty collection then the operator has no
             input ports. Otherwise a list or tuple of ``Stream`` instances
             where the number of items is the number of input ports.
         schemas: Schemas of the output ports. If not set or set to
-            ``None`` or an empty collection then the operator has no
+            `None` or an empty collection then the operator has no
             outut ports. Otherwise a list or tuple of schemas
             where the number of items is the number of output ports.
         params: Operator parameters.
-        name: Name of the operator. When ``None`` defaults to a name
+        name: Name of the operator. When `None` defaults to a name
             derived from the operator kind.
              
     """
@@ -231,8 +231,7 @@ class Source(Invoke):
         kind(str): SPL operator kind, e.g. ``spl.utility::Beacon``.
         schema: Schema of the output port.
         params: Operator parameters.
-        name: Name of the operator. When ``None`` defaults to a name
-            derived from the operator kind.
+        name: Name of the operator. When `None` defaults to a generated name.
     """
     def __init__(self,topology,kind,schema,params=None,name=None):
         super(Source,self).__init__(topology, kind, schemas=schema, params=params,name=name)
@@ -278,8 +277,7 @@ class Map(Invoke):
         stream: Stream to connect to the operator.
         schema: Schema of the output stream. If set to `None` then the output schema is the same as the schema of `stream`.
         params: Operator parameters.
-        name: Name of the operator. When ``None`` defaults to a name
-            derived from the operator kind.
+        name: Name of the operator. When `None` defaults to a generated name.
     """
     def __init__(self,kind,stream,schema=None,params=None,name=None):
         if schema is None:
@@ -336,8 +334,7 @@ class Sink(Invoke):
         kind(str): SPL operator kind, e.g. ``spl.adapter::FileSink``.
         input: Stream to connect to the operator.
         params: Operator parameters.
-        name: Name of the operator. When ``None`` defaults to a name
-            derived from the operator kind.
+        name: Name of the operator. When `None` defaults to a generated name.
     """
     def __init__(self,kind,stream,params=None,name=None):
         super(Sink,self).__init__(stream.topology,kind,inputs=stream,params=params,name=name)
