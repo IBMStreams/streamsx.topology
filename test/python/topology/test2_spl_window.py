78db5c357935e33ec7b6686f46ab08025b104f07
Daniel J. Debrunner <debrunne@us.ibm.com>
2017-05-23 14:39:30 -0400

    Initial Window class

---
+++ test/python/topology/test2_spl_window.py
@@ -0,0 +1,44 @@
+# Licensed Materials - Property of IBM
+# Copyright IBM Corp. 2016
+import unittest
+import sys
+import itertools
+
+import test_vers
+
+from streamsx.topology.topology import *
+from streamsx.topology.tester import Tester
+from streamsx.topology import schema
+import streamsx.topology.context
+import streamsx.spl.op as op
+
+
+@unittest.skipIf(not test_vers.tester_supported() , "tester not supported")
+class TestSPLWindow(unittest.TestCase):
+    """ Test invocations of SPL operators from Python topology.
+    """
+    def setUp(self):
+        Tester.setup_standalone(self)
+
+    def test_sliding_count(self):
+        for step in [1, 3]:
+            with self.subTest(step=step):
+                topo = Topology()
+                b = op.Source(topo, "spl.utility::Beacon",
+                    'tuple<uint64 seq>',
+                    params = {'iterations':12})
+                b.seq = b.output('IterationCount()')
+                s = b.stream
+ 
+                agg = op.Map('spl.relational::Aggregate', s.last(4).trigger(step),
+                    schema = 'tuple<uint64 sum, uint64 max>')
+                agg.sum = agg.output('Sum(seq)')
+                agg.max = agg.output('Max(seq)')
+ 
+                expected = []
+                for i in range(4 + step - 2, 12, step):
+                    expected.append({'sum': sum(range(i-3, i+1)), 'max': i})
+
+                tester = Tester(topo)
+                tester.contents(agg.stream, expected)
+                tester.test(self.test_ctxtype, self.test_config)
