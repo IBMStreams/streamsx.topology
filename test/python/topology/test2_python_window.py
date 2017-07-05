9c728ba4d4b7eb6ebd775a268b978ff17dde52ae
Will <wmarshall484@gmail.com>
2017-07-04 20:05:11 -0400

    Added windowing test which uses Python classes

--- test/python/topology/test2_python_window.py
+++ test/python/topology/test2_python_window.py
@@ -3,6 +3,14 @@ import unittest
 from streamsx.topology.topology import *
 from streamsx.topology.tester import Tester
 
+class Person(object):
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def rough_birth_year(self):
+        return time.localtime().tm_year - self.age;
+
 class TestPythonWindowing(unittest.TestCase):
     def setUp(self):
         Tester.setup_standalone(self)
@@ -14,6 +22,24 @@ class TestPythonWindowing(unittest.TestCase):
         
         tester = Tester(topo)
         tester.contents(s, [1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5, 6.5, 7.5, 8.5, 9.5, 10.5])
+        tester.test(self.test_ctxtype, self.test_config)
+
+    def test_ClassCountCountWindow(self):
+        topo = Topology()
+        s = topo.source([
+                ['Wallace', 55],
+                ['Copernicus', 544],
+                ['Feynman', 99],
+                ['Dirac', 115],
+                ['Pauli', 117],
+                ['Frenkel', 49],
+                ['Terence Tao', 42]
+        ])
+        s = s.map(lambda x: Person(x[0], x[1]))
+        s = s.last(3).trigger(1).aggregate(lambda x: int(sum([p.rough_birth_year() for p in x])/len(x)))
+
+        tester = Tester(topo)
+        tester.contents(s, [1962, 1717, 1784, 1764, 1906, 1923, 1947])
         tester.test(self.test_ctxtype, self.test_config)
 
 if __name__ == '__main__':
