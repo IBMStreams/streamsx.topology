03fcc2c7886b43fca7c55f9710c7eab139023044
Dan Debrunner <debrunne@us.ibm.com>
2017-07-26 13:00:29 -0700

    Move extract to scripts

--- com.ibm.streamsx.topology/bin/spl-python-extract.py
+++ com.ibm.streamsx.topology/bin/spl-python-extract.py
@@ -1,7 +1,7 @@
 # Licensed Materials - Property of IBM
 # Copyright IBM Corp. 2016,2017
 import sys
-import streamsx.spl.extract as sse
+import streamsx.scripts.extract as sse
 
 sse._extract_from_toolkit()
 sys.exit(0)
