f6dba4f4760382545cf461dc7fea25f173f4c4a8
Will <wmarshall484@gmail.com>
2017-08-29 01:44:50 -0400

    Convert pickled value to python object in CCWindow

--- test/python/topology/test2_python_window.py
+++ test/python/topology/test2_python_window.py
@@ -41,7 +41,20 @@ class TestPythonWindowing(unittest.TestCase):
         topo = Topology()
         s = topo.source(['1','3','5','7'])
         s = s.map(lambda x: x, schema = CommonSchema.String)
-        #s = s.last(3).trigger(1).aggregate(lambda x: int(sum([int(s) for s in x])/len(x)))
+        s = s.last(3).trigger(1).aggregate(lambda x: int(sum([int(s) for s in x])/len(x)),
+                                           schema = CommonSchema.Python)
+
+        tester = Tester(topo)
+        tester.contents(s, [1,2,3,5])
+        tester.test(self.test_ctxtype, self.test_config)
+
+    def test_NotByRefWindow(self):
+        topo = Topology()
+        s = topo.source(['1','3','5','7'])
+
+        # Used to prevent pass by ref for the source
+        f = s.filter(lambda x: True)
+
         s = s.last(3).trigger(1).aggregate(lambda x: int(sum([int(s) for s in x])/len(x)),
                                            schema = CommonSchema.Python)
 
