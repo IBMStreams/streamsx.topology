33eb2f8487f007cef9a6549d2dfc980ee16b5b33
Will <wmarshall484@gmail.com>
2017-08-31 10:40:52 -0400

    Change trigger of BasicCountCountWindow test to 2

--- test/python/topology/test2_python_window.py
+++ test/python/topology/test2_python_window.py
@@ -19,10 +19,10 @@ class TestPythonWindowing(unittest.TestCase):
     def test_BasicCountCountWindow(self):
         topo = Topology()
         s = topo.source([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15])
-        s = s.last(10).trigger(1).aggregate(lambda x: sum(x)/len(x))
-        
+        s = s.last(10).trigger(2).aggregate(lambda x: sum(x)/len(x))
+
         tester = Tester(topo)
-        tester.contents(s, [1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5, 6.5, 7.5, 8.5, 9.5, 10.5])
+        tester.contents(s, [1.5,2.5,3.5,4.5,5.5,7.5,9.5])
         tester.test(self.test_ctxtype, self.test_config)
 
     def test_JsonInputCountCountWindow(self):
