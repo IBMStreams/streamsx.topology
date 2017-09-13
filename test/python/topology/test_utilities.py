041efa22b0f16f383d4b5350778494fdc37d29ec
Dan Debrunner <debrunne@us.ibm.com>
2017-09-13 07:13:35 -0700

    Skip Python standalone (older) tests is no STREAMS_INSTALL

--- test/python/topology/test_utilities.py
+++ test/python/topology/test_utilities.py
@@ -1,9 +1,14 @@
 # Licensed Materials - Property of IBM
 # Copyright IBM Corp. 2017
+import os
 import sys
 import streamsx.topology.context
 
 def standalone(test, topo):
+    if 'STREAMS_INSTALL' not in os.environ:
+        test.skipTest("No STREAMS_INSTALL")
+    if not os.environ['STREAMS_INSTALL']:
+        test.skipTest("STREAMS_INSTALL empty")
     rc = streamsx.topology.context.submit("STANDALONE", topo)
     test.assertEqual(0, rc['return_code'])
 
