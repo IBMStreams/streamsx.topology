9606518a4aadcdd66d61feee52425ea220a0b5f5
Dan Debrunner <debrunne@us.ibm.com>
2017-09-13 07:14:34 -0700

    Simplify check

--- test/python/topology/test_utilities.py
+++ test/python/topology/test_utilities.py
@@ -7,8 +7,6 @@ import streamsx.topology.context
 def standalone(test, topo):
     if 'STREAMS_INSTALL' not in os.environ:
         test.skipTest("No STREAMS_INSTALL")
-    if not os.environ['STREAMS_INSTALL']:
-        test.skipTest("STREAMS_INSTALL empty")
     rc = streamsx.topology.context.submit("STANDALONE", topo)
     test.assertEqual(0, rc['return_code'])
 
