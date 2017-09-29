8fdfbe0a0d713b2b1cd24c12bdcdc978bfb1a840
Dan Debrunner <debrunne@us.ibm.com>
2017-09-29 10:56:24 -0700

    Add toolkit to test mix of SPL toolkits with older decorated ops

--- test/python/spl/tk17/com.ibm.streamsx.topology.pytest.tk17/M17/splpy_operator.pm
+++
@@ -1,8 +0,0 @@
-sub splpy_Module { 'ops'}
-sub splpy_OperatorCallable {'function'}
-sub splpy_FunctionName {'M17'}
-sub splpy_OperatorType {'Pipe'}
-sub splpy_FixedParam { '-1'}
-sub splpy_ParamStyle { 'tuple'}
-sub splpy_Packages {()}
-1;
