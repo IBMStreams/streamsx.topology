0785d602d157a6a26ed7f3a9c2b16e94ecb7a0ba
Dan Debrunner <debrunne@us.ibm.com>
2017-10-05 13:57:51 -0700

    Have a consistent method for calling PyObject_Call

---
+++ test/python/spl/testtkpy/opt/python/streams/test_kwargs_ops.py
@@ -0,0 +1,33 @@
+# Licensed Materials - Property of IBM
+# Copyright IBM Corp. 2017
+
+# Import the SPL decorators
+from streamsx.spl import spl
+
+#------------------------------------------------------------------
+# Test Map functions
+#------------------------------------------------------------------
+
+# Defines the SPL namespace for any functions in this module
+# Multiple modules can map to the same namespace
+def splNamespace():
+    return "com.ibm.streamsx.topology.pytest.pykwargs"
+
+@spl.filter()
+def KWFilter(**t):
+    a = t['a']
+    b = t['b']
+    c = t['c']
+
+    return a < b and c > 100
+
+
+@spl.for_each()
+def KWForEach(**t):
+    a = t['a']
+    b = t['b']
+    c = t['c']
+    if a >= b:
+        raise ValueError("A>=B" + str(t))
+    if c <= 100:
+        raise ValueError("C<=100" + str(t))
