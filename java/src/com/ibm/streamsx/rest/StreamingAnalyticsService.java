ce0fe7c6033e9fecf8d54044f12f08577da855d7
John MacMillan <johnmac@ca.ibm.com>
2017-11-22 09:11:44 -0500

    [Fixes #1305] Fixes in StreamingAnalyticsService
    - buildAndSubmitJob now returns BigInteger
    - Javadoc improvements

--- java/src/com/ibm/streamsx/rest/StreamingAnalyticsService.java
+++ java/src/com/ibm/streamsx/rest/StreamingAnalyticsService.java
@@ -12,17 +12,20 @@ import java.math.BigInteger;
 import com.google.gson.JsonObject;
 
 /**
- * An interface for using the REST API of Bluemix Streaming Analytics Service.
+ * An interface for using the REST API of IBM Clould Streaming Analytics Service.
  * Not all REST services are supported at this time, only the ones currently
- * used by Topology around build and job submission.
+ * used by Topology for build and job submission.
+ * @since 1.8
  */
 public interface StreamingAnalyticsService {
     /**
      * Submit a Streams bundle to run on the Streaming Analytics Service.
+     * <p>The JSON object may contain an optional {@code deploy} member that
+     * includes deployment information.
      * @param bundle A streams application bundle
-     * @param submission Submission parameters, may have results added.
-     * @return The job number. The submission results will also be added to the
-     * submission parameters object.
+     * @param submission Deployment info to be submitted.
+     * @return The job id, or -1. Results from the submit will be added to the
+     * submission parameter object as a member named @{code submissionResults}.
      * @throws IOException
      */
     BigInteger submitJob(File bundle, JsonObject submission) throws IOException;
@@ -31,10 +34,18 @@ public interface StreamingAnalyticsService {
      * Submit an archive to build on the Streaming Analytics Service, and submit
      * the job if the build is successful.
      * <p>
+     * The JSON object contains two keys:
+     * <UL>
+     * <LI>{@code deploy} - Optional - Deployment information.</LI>
+     * <LI>{@code graph} - Required - JSON representation of the topology graph.</LI>
+     * </UL>
+     * <p>
      * Results are added to the submission parameters object.
      * @param archive The application archive to build.
-     * @param submission Submission parameters, may have results added.
+     * @param submission Topology and deployment info to be submitted.
+     * @return The job id, or -1. Results from the submit will be added to the
+     * submission parameter object as a member named @{code submissionResults}.
      * @throws IOException
      */
-    void buildAndSubmitJob(File archive, JsonObject submission) throws IOException;
+    BigInteger buildAndSubmitJob(File archive, JsonObject submission) throws IOException;
 }
