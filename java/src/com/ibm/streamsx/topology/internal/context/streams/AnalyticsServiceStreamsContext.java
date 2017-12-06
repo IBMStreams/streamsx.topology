82cccfdf8b34e30ca06cedf934e72426fab62ae3
Dan Debrunner <debrunne@us.ibm.com>
2017-12-06 10:17:18 -0800

    Fix #1342 - Honor forceRemoteBuild

--- java/src/com/ibm/streamsx/topology/internal/context/streams/AnalyticsServiceStreamsContext.java
+++ java/src/com/ibm/streamsx/topology/internal/context/streams/AnalyticsServiceStreamsContext.java
@@ -4,9 +4,11 @@
  */
 package com.ibm.streamsx.topology.internal.context.streams;
 
+import static com.ibm.streamsx.topology.context.ContextProperties.FORCE_REMOTE_BUILD;
 import static com.ibm.streamsx.topology.internal.context.remote.DeployKeys.deploy;
 import static com.ibm.streamsx.topology.internal.context.remote.DeployKeys.keepArtifacts;
 import static com.ibm.streamsx.topology.internal.context.remote.RemoteBuildAndSubmitRemoteContext.streamingAnalyticServiceFromDeploy;
+import static com.ibm.streamsx.topology.internal.gson.GsonUtilities.jboolean;
 import static com.ibm.streamsx.topology.internal.streaminganalytics.VcapServices.getVCAPService;
 
 import java.io.File;
@@ -18,10 +20,13 @@ import com.google.gson.JsonObject;
 import com.ibm.streamsx.rest.Job;
 import com.ibm.streamsx.rest.Result;
 import com.ibm.streamsx.rest.StreamingAnalyticsService;
+import com.ibm.streamsx.topology.context.ContextProperties;
 import com.ibm.streamsx.topology.context.remote.RemoteContext;
+import com.ibm.streamsx.topology.internal.context.JSONStreamsContext.AppEntity;
 import com.ibm.streamsx.topology.internal.context.remote.DeployKeys;
 import com.ibm.streamsx.topology.internal.context.remote.RemoteContexts;
 import com.ibm.streamsx.topology.internal.context.remote.SubmissionResultsKeys;
+import com.ibm.streamsx.topology.internal.context.service.RemoteStreamingAnalyticsServiceStreamsContext;
 import com.ibm.streamsx.topology.internal.gson.GsonUtilities;
 import com.ibm.streamsx.topology.internal.process.CompletedFuture;
 
@@ -39,6 +44,17 @@ public class AnalyticsServiceStreamsContext extends
     public Type getType() {
         return type;
     }
+    
+    @Override
+    protected Future<BigInteger> action(AppEntity entity) throws Exception {
+        if (jboolean(deploy(entity.submission), FORCE_REMOTE_BUILD)) {
+            RemoteStreamingAnalyticsServiceStreamsContext rc = new RemoteStreamingAnalyticsServiceStreamsContext();
+            return rc.submit(entity.submission);
+        }
+
+        return super.action(entity);
+    }
+    
     
     @Override
     Future<BigInteger> invoke(AppEntity entity, File bundle) throws Exception {
