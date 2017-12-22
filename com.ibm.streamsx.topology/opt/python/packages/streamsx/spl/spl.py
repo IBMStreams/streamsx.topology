f6d649de55ec3755507761c89302507a1a5d7bed
Dan Debrunner <debrunne@us.ibm.com>
2017-12-21 16:16:39 -0800

    Clean up summaries and levels

--- com.ibm.streamsx.topology/opt/python/packages/streamsx/spl/spl.py
+++ com.ibm.streamsx.topology/opt/python/packages/streamsx/spl/spl.py
@@ -26,14 +26,14 @@ These are the supported decorators that create an SPL operator:
     * :py:class:`@spl.filter <filter>` - Creates a operator that filters tuples.
     * :py:class:`@spl.map <map>` - Creates a operator that maps input tuples to output tuples.
     * :py:class:`@spl.for_each <for_each>` - Creates a operator that terminates a stream processing each tuple.
-    * :py:class:`@spl.primitive_operator <primitive_operator>` - Creates an SPL primitive operator that
-        has an an arbitrary number of input and output ports.
+    * :py:class:`@spl.primitive_operator <primitive_operator>` - Creates an SPL primitive operator that has an arbitrary number of input and output ports.
 
+*******************************
 Python classes as SPL operators
-===============================
+*******************************
 
 Overview
---------
+========
 
 Decorating a Python class creates a stateful SPL operator
 where the instance fields of the class are the operator's state. An instance
@@ -100,7 +100,7 @@ or both operator parameters can be set::
     }
 
 Operator state
---------------
+==============
 
 Use of a class allows the operator to be stateful by maintaining state in instance
 attributes across invocations (tuple processing).
@@ -110,7 +110,7 @@ attributes across invocations (tuple processing).
     state can be pickled. See https://docs.python.org/3.5/library/pickle.html#handling-stateful-objects
 
 Operator initialization & shutdown
-----------------------------------
+==================================
 
 Execution of an instance for an operator effectively run in a context manager so that an instance's ``__enter__``
 method is called when the processing element containing the operator is initialized
@@ -139,15 +139,17 @@ Example of using ``__enter__`` and ``__exit__`` to open and close a file::
                 self.file.close()
 
 Application log and trace
--------------------------
+=========================
 
 IBM Streams provides application trace and log services which are
 accesible through standard Python loggers from the `logging` module.
 
 See :ref:`streams_app_log_trc`.
 
+*********************************
 Python functions as SPL operators
-=================================
+*********************************
+
 Decorating a Python function creates a stateless SPL operator.
 In SPL terms this is similar to an SPL Custom operator, where
 the code in the Python function is the custom code. For
@@ -168,20 +170,21 @@ its conversion to a Python tuple::
 
 .. _spl-tuple-to-python:
 
+*******************************
 Processing SPL tuples in Python
-===============================
+*******************************
 
 SPL tuples are converted to Python objects and passed to a decorated callable.
 
 Overview
---------
+========
 
 For each SPL tuple arriving at an input port a Python function is called with
 the SPL tuple converted to Python values suitable for the function call.
 How the tuple is passed is defined by the tuple passing style.
 
 Tuple Passing Styles
---------------------
+====================
 
 An input tuple can be passed to Python function using a number of different styles:
  * *dictionary*
@@ -190,7 +193,7 @@ An input tuple can be passed to Python function using a number of different styl
  * *attributes by position*
 
 Dictionary
-++++++++++
+----------
 
 Passing the SPL tuple as a Python dictionary is flexible
 and makes the operator independent of any schema.
@@ -207,7 +210,7 @@ All of the attributes are passed in the dictionary
 using the SPL schema attribute name as the key.
 
 Tuple
-+++++
+-----
 
 Passing the SPL tuple as a Python tuple is flexible
 and makes the operator independent of any schema
@@ -224,7 +227,7 @@ All of the attributes are passed as a Python tuple
 with the order of values matching the order of the SPL schema.
 
 Attributes by name
-++++++++++++++++++
+------------------
 (**not yet implemented**)
 
 Passing attributes by name can be robust against changes
@@ -251,7 +254,7 @@ If there are only formal parameters any non-bound attributes
 are not passed into the function.
 
 Attributes by position
-++++++++++++++++++++++
+----------------------
 
 Passing attributes by position allows the SPL operator to
 be independent of the SPL schema but is brittle to
@@ -279,7 +282,7 @@ The SPL schema must have at least the number of positional arguments
 the function requires.
 
 Selecting the style
-+++++++++++++++++++
+===================
 
 For signatures only containing a parameter of the form 
 ``*args`` or ``**kwargs`` the style is implicitly defined:
@@ -297,7 +300,7 @@ defaulting to *attributes by name*. The style value can be set to:
 They do not support the ``style`` parameter.
 
 Examples
-++++++++
+========
 
 These examples show how an SPL tuple with the schema and value::
 
@@ -416,8 +419,9 @@ remaining attributes are optional (having a default).
 
 .. _submit-from-python:
 
+************************************
 Submission of SPL tuples from Python
-------------------------------------
+************************************
 
 The return from a decorated callable results in submission of SPL tuples
 on the associated outut port.
@@ -429,13 +433,13 @@ A Python function must return:
  * a list containing any of the above.
 
 None
-++++
+====
 
 When ``None`` is return then no tuple will be submitted to
 the operator output port.
 
 Python tuple
-++++++++++++
+============
 
 When a Python tuple is returned it is converted to an SPL tuple and
 submitted to the output port.
@@ -506,7 +510,8 @@ When a returned tuple has more values than attributes in the SPL output schema t
     # The fourth value in the tuple a/b = x/y is ignored.
 
 Python dictionary
-+++++++++++++++++
+=================
+
 A Python dictionary is converted to an SPL tuple for submission to
 the associated output port. An SPL attribute is set from the
 dictionary if the dictionary contains a key equal to the attribute
@@ -521,7 +526,8 @@ default value depending on the operator kind.
 Any keys in the dictionary that do not map to SPL attribute names are ignored.
     
 Python list
-+++++++++++
+===========
+
 When a list is returned, each value is converted to an SPL tuple and
 submitted to the output port, in order of the list starting with the
 first element (position 0). If the list contains `None` at an index
