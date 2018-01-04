571a848a0424d4703f852a8dfacbf276796a2ccc
Dan Debrunner <debrunne@us.ibm.com>
2018-01-03 16:02:54 -0800

    Allow test to have class specific tests on tuple type

--- test/python/topology/test2_schema_tuple.py
+++ test/python/topology/test2_schema_tuple.py
@@ -14,44 +14,58 @@ from streamsx.topology.tester import Tester
 Test that structured schemas can be passed into Python functions as tuples.
 """
 
-def check_is_tuple(t):
-    if type(t) != tuple:
+def check_correct(named, t):
+    if named:
+        if not isinstance(t, tuple):
+            raise ValueError("Expected a tuple:" + str(t) + " >> type:" + str(type(t)))
+    elif type(t) != tuple:
         raise ValueError("Expected a tuple:" + str(t) + " >> type:" + str(type(t)))
 
-def check_is_tuple_for_each(t):
-    check_is_tuple(t)
+class check_for_tuple_type(object):
+  def __init__(self, named):
+     self.named = named
+
+class check_is_tuple_for_each(check_for_tuple_type):
+  def __call__(self, t):
+    check_correct(self.named, t)
     if t[1] != str(t[0]*2) + "Hi!":
         raise ValueError("Incorrect value:" + str(t))
 
-def check_is_tuple_filter(t):
-    check_is_tuple_for_each(t)
+class check_is_tuple_filter(check_is_tuple_for_each):
+  def __call__(self, t):
+    super(check_is_tuple_filter, self).__call__(t)
     return t[0] != 2
 
 def check_is_tuple_hash(t):
-    print("ISHASH:", t, flush=True)
-    check_is_tuple_for_each(t)
-    print("ISHASH_RET:", t[0], flush=True)
+    check_correct(False, t)
+    if t[1] != str(t[0]*2) + "Hi!":
+        raise ValueError("Incorrect value:" + str(t))
     return t[0]
 
-def check_is_tuple_map(t):
-    check_is_tuple(t)
+class check_is_tuple_map(check_for_tuple_type):
+  def __call__(self, t):
+    check_correct(self.named, t)
     return (t[0]*7, t[1] + "-Map")
 
 
-def check_is_tuple_map_to_string(t):
-    check_is_tuple(t)
+class check_is_tuple_map_to_string(check_for_tuple_type):
+  def __call__(self, t):
+    check_correct(self.named, t)
     return str(t[0]*11) + t[1] + "-MapString"
 
-def check_is_tuple_map_to_schema(t):
-    check_is_tuple(t)
+class check_is_tuple_map_to_schema(check_for_tuple_type):
+  def __call__(self, t):
+    check_correct(self.named, t)
     return (t[0]*13, t[1] + "-MapSPL")
 
-def check_is_tuple_flat_map(t):
-    check_is_tuple(t)
+class check_is_tuple_flat_map(check_for_tuple_type):
+  def __call__(self, t):
+    check_correct(self.named, t)
     return [(t[0]*9, t[1] + "-FlatMap")]
 
-def check_is_tuple_map_to_json(t):
-    check_is_tuple(t)
+class check_is_tuple_map_to_json(check_for_tuple_type):
+  def __call__(self, t):
+    check_correct(self.named, t)
     return {'a':t[0]*15, 'b':t[1] + "-MapJSON"}
 
 @unittest.skipIf(not test_vers.tester_supported() , "tester not supported")
@@ -61,6 +75,12 @@ class TestSchemaTuple(unittest.TestCase):
     def setUp(self):
         Tester.setup_standalone(self)
 
+    def is_named(self):
+        return False
+
+    def hash_check(self):
+        return check_is_tuple_hash
+
     def _create_stream(self, topo):
         s = topo.source([1,2,3])
         schema=StreamSchema('tuple<int32 x, rstring msg>').as_tuple()
@@ -69,7 +89,7 @@ class TestSchemaTuple(unittest.TestCase):
     def test_as_tuple_for_each(self):
         topo = Topology()
         st = self._create_stream(topo)
-        st.for_each(check_is_tuple_for_each)
+        st.for_each(check_is_tuple_for_each(self.is_named()))
 
         tester = Tester(topo)
         tester.tuple_count(st, 3)
@@ -78,7 +98,7 @@ class TestSchemaTuple(unittest.TestCase):
     def test_as_tuple_map(self):
         topo = Topology()
         s = self._create_stream(topo)
-        st = s.map(check_is_tuple_map)
+        st = s.map(check_is_tuple_map(self.is_named()))
 
         tester = Tester(topo)
         tester.contents(st, [(7,'2Hi!-Map'), (14,'4Hi!-Map'), (21,'6Hi!-Map')])
@@ -87,7 +107,7 @@ class TestSchemaTuple(unittest.TestCase):
     def test_as_tuple_filter(self):
         topo = Topology()
         s = self._create_stream(topo)
-        s = s.filter(check_is_tuple_filter)
+        s = s.filter(check_is_tuple_filter(self.is_named()))
 
         tester = Tester(topo)
         tester.tuple_count(s, 2)
@@ -96,7 +116,7 @@ class TestSchemaTuple(unittest.TestCase):
     def test_as_tuple_flat_map(self):
         topo = Topology()
         s = self._create_stream(topo)
-        st = s.flat_map(check_is_tuple_flat_map)
+        st = s.flat_map(check_is_tuple_flat_map(self.is_named()))
 
         tester = Tester(topo)
         tester.contents(st, [(9,'2Hi!-FlatMap'), (18,'4Hi!-FlatMap'), (27,'6Hi!-FlatMap')])
@@ -105,8 +125,8 @@ class TestSchemaTuple(unittest.TestCase):
     def test_as_tuple_hash(self):
         topo = Topology()
         s = self._create_stream(topo)
-        s = s.parallel(width=2, routing=Routing.HASH_PARTITIONED, func=check_is_tuple_hash)
-        s = s.map(check_is_tuple_map)
+        s = s.parallel(width=2, routing=Routing.HASH_PARTITIONED, func=self.hash_check())
+        s = s.map(check_is_tuple_map(self.is_named()))
         s = s.end_parallel()
 
         tester = Tester(topo)
@@ -116,7 +136,7 @@ class TestSchemaTuple(unittest.TestCase):
     def test_as_tuple_map_to_string(self):
         topo = Topology()
         s = self._create_stream(topo)
-        st = s.map(check_is_tuple_map_to_string, schema=CommonSchema.String)
+        st = s.map(check_is_tuple_map_to_string(self.is_named()), schema=CommonSchema.String)
 
         tester = Tester(topo)
         tester.contents(st, ['112Hi!-MapString', '224Hi!-MapString', '336Hi!-MapString'])
@@ -125,7 +145,7 @@ class TestSchemaTuple(unittest.TestCase):
     def test_as_tuple_map_to_schema(self):
         topo = Topology()
         s = self._create_stream(topo)
-        st = s.map(check_is_tuple_map_to_schema, schema=StreamSchema('tuple<int32 y, rstring txt>'))
+        st = s.map(check_is_tuple_map_to_schema(self.is_named()), schema=StreamSchema('tuple<int32 y, rstring txt>'))
 
         tester = Tester(topo)
         tester.contents(st, [{'y':13, 'txt':'2Hi!-MapSPL'}, {'y':26, 'txt':'4Hi!-MapSPL'}, {'y':39, 'txt':'6Hi!-MapSPL'}])
@@ -134,7 +154,7 @@ class TestSchemaTuple(unittest.TestCase):
     def test_as_tuple_map_to_json(self):
         topo = Topology()
         s = self._create_stream(topo)
-        s = s.map(check_is_tuple_map_to_json, schema=CommonSchema.Json)
+        s = s.map(check_is_tuple_map_to_json(self.is_named()), schema=CommonSchema.Json)
         s = s.map(lambda x : str(x['a']) + x['b'])
 
         tester = Tester(topo)
@@ -144,6 +164,9 @@ class TestSchemaTuple(unittest.TestCase):
 
 @unittest.skipIf(not test_vers.tester_supported() , "tester not supported")
 class TestSchemaNamedTuple(TestSchemaTuple):
+    def is_named(self):
+        return True
+
     def _create_stream(self, topo):
         s = topo.source([1,2,3])
         schema=StreamSchema('tuple<int32 x, rstring msg>').as_tuple(named=True)
