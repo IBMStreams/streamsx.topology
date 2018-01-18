58a8ba17d4bc3ac0fc6237d4c25f51b89990ebdd
Will Marshall <wmarshall484@gmail.com>
2018-01-18 03:27:09 -0800

    SAS correctly returns json response from commands

--- com.ibm.streamsx.topology/opt/python/packages/streamsx/rest_primitives.py
+++ com.ibm.streamsx.topology/opt/python/packages/streamsx/rest_primitives.py
@@ -1279,7 +1279,7 @@ class StreamingAnalyticsService(object):
         Returns:
             dict: JSON response for the job cancel operation.
         """
-        self._delegator.cancel_job(job_id=job_id, job_name = job_name)
+        return self._delegator.cancel_job(job_id=job_id, job_name = job_name)
 
     def start_instance(self):
         """Start the instance for this Streaming Analytics service.
@@ -1287,7 +1287,7 @@ class StreamingAnalyticsService(object):
         Returns:
             dict: JSON response for the instance start operation.
         """
-        self._delegator.start_instance()
+        return self._delegator.start_instance()
 
     def stop_instance(self):
         """Stop the instance for this Streaming Analytics service.
@@ -1295,7 +1295,7 @@ class StreamingAnalyticsService(object):
         Returns:
             dict: JSON response for the instance start operation.
         """
-        self._delegator.stop_instance()
+        return self._delegator.stop_instance()
 
     def get_instance_status(self):
         """Get the status the instance for this Streaming Analytics service.
@@ -1342,6 +1342,7 @@ class _StreamingAnalyticsServiceV2Delegator(object):
         prepared = req.prepare()
         res = self.rest_client.session.send(prepared)
         self.rest_client.handle_http_errors(res)
+        return res.json()
 
     def start_instance(self):
         req = requests.Request("PATCH", self._v2_rest_url, json={'state' : 'STARTED'},
@@ -1351,6 +1352,7 @@ class _StreamingAnalyticsServiceV2Delegator(object):
         prepared = req.prepare()
         res = self.rest_client.session.send(prepared)
         self.rest_client.handle_http_errors(res)
+        return res.json()
 
     def stop_instance(self):
         req = requests.Request("PATCH", self._v2_rest_url, json={'state' : 'STOPPED'},
@@ -1360,6 +1362,7 @@ class _StreamingAnalyticsServiceV2Delegator(object):
         prepared = req.prepare()
         res = self.rest_client.session.send(prepared)
         self.rest_client.handle_http_errors(res)
+        return res.json()
 
     def get_instance_status(self):
         return self.rest_client.make_request(self._v2_rest_url)
