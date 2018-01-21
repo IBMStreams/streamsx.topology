2851ed8ec864ff39af1e33074e631d1fa80b0e9c
Dan Debrunner <ddebrunner@users.noreply.github.com>
2018-01-21 10:46:23 -0800

    Fix #1422 and change Timestamp to be a namedtuple (#1423)
    * Fix #1422 and change Timestamp to be a namedtuple
    
    * Add testing of nanoseconds validatio

--- com.ibm.streamsx.topology/opt/python/packages/streamsx/spl/types.py
+++ com.ibm.streamsx.topology/opt/python/packages/streamsx/spl/types.py
@@ -10,27 +10,48 @@ must use the SPL type required by the operator.
 
 """
 
+import collections
 import datetime
+import time
+import streamsx.spl.op
 
-class Timestamp(object):
+class Timestamp(collections.namedtuple('Timestamp', ['seconds', 'nanoseconds', 'machine_id'])):
     """
-    SPL naive timestamp type with nanosecond resolution.
+    SPL native timestamp type with nanosecond resolution.
 
     Common usage is to store the seconds and nanoseconds since the Unix Epoch (Jan 1, 1970),
     but this is not enforced by the `Timestamp` class.
 
-    Machine identifier is an application defined identifier for the machine the timestamp
+    Machine identifier is an optional application defined identifier for the machine the timestamp
     was created on. It is the responsibility of the application to set the machine identifier
     if required. The machine identifier may be used to detect if two timestamps were created on the same machine,
     as there may be variations in the clocks on different machines.
 
+    A instance can be created by passing seconds, nanoseconds and
+    optionally machine identifier::
+
+        # Timestamp with the current time in seconds
+        # discarding any fractional seconds.
+        ts = Timestamp(time.time(), 0)
+
+        # Timestamp set to a specific time with a machine identifier
+        ts = Timestamp(1516500542, 9511447, 4)
+
+    A `Timestamp` is a namedtuple with three fields `seconds`, `nanoseconds`
+    and `machine_id`.
+
     Attributes:
         seconds (int) : Seconds since epoch.
         nanoseconds (int) : Nanosecond component.
-        machine_id (int) : Machine identifier.
+        machine_id (int) : Optional machine identifier, defaults to zero.
+
+    .. warning::
+        Implementation of `Timestamp` changed with 1.8.3 to be a `namedtuple`
+        maintaining the existing class API.
     """
 
     _EPOCH = datetime.datetime.utcfromtimestamp(0)
+    _NS = 1000000000.0
 
     @staticmethod
     def from_datetime(dt, machine_id=0):
@@ -41,36 +62,53 @@ class Timestamp(object):
            machine_id(int): Machine identifier.
 
         Returns:
-             Timestamp: SPL timestamp value.
+             Timestamp: Datetime converted to Timestamp.
         """
         td = dt - Timestamp._EPOCH
         seconds = td.days * 3600 * 24
         seconds += td.seconds
         return Timestamp(seconds, td.microseconds*1000, machine_id)
 
-    def __init__(self, seconds, nanoseconds, machine_id=0):
-        self._seconds = int(seconds)
-        self._nanoseconds = int(nanoseconds)
-        self._machine_id = int(machine_id)
+    @staticmethod
+    def from_time(t, machine_id=0):
+        """Convert seconds since epoch to a Timestamp.
 
-    @property
-    def seconds(self):
-        """
-        Seconds value.
+        The time argument matches the return from ``time.time()``.
+
+        Args:
+           t(float): Time to be converted.
+           machine_id(int): Machine identifier.
 
         Returns:
-            int: Seconds value.
+             Timestamp: Time converted to Timestamp.
 
+        .. versionadded:: 1.8.3
         """
-        return self._seconds
+        return Timestamp(t, (t % 1) * Timestamp._NS, machine_id)
 
-    @property
-    def nanoseconds(self):
-        return self._nanoseconds
+    @staticmethod
+    def now(machine_id=0):
+        """Timestamp representing the current time.
 
-    @property
-    def machine_id(self):
-        return self._machine_id
+        Args:
+           machine_id(int): Machine identifier.
+
+        Returns:
+             Timestamp: Current time.
+
+        .. versionadded:: 1.8.3
+        """
+        return Timestamp.from_time(time.time(), machine_id)
+
+    @staticmethod
+    def _check_nanos(ns):
+         ns = int(ns)
+         if ns < 0 or ns >= Timestamp._NS:
+              raise ValueError("nanoseconds must in the range 0-999999999")
+         return ns
+
+    def __new__(cls, seconds, nanoseconds, machine_id=0):
+        return super().__new__(cls, int(seconds), Timestamp._check_nanos(nanoseconds), int(machine_id))
 
     def time(self):
         """
@@ -79,17 +117,17 @@ class Timestamp(object):
         Returns:
             float: time in seconds since the epoch.
         """
-        return self.seconds + (self.nanoseconds / 1000000000.0)
+        return self.seconds + (self.nanoseconds / Timestamp._NS)
 
     def datetime(self):
         """
         Return the UTC datetime corresponding to the POSIX timestamp.
 
-        This is identical to ```datetime.datetime.utcfromtimestamp(self.time())```.
+        This is identical to ``datetime.datetime.utcfromtimestamp(self.time())``.
         Nanosecond resolution may be lost.
 
         Returns:
-             (datetime.datetime):
+             datetime.datetime: Timestamp converted to a `datetime.datetime`.
         """
         return datetime.datetime.utcfromtimestamp(self.time())
 
@@ -99,13 +137,10 @@ class Timestamp(object):
         Returns:
             tuple: Returns a tuple of ``(seconds, nanoseconds, machine_id)``
 
+        .. deprecated:: 1.8.3
+            Timestamp is a `tuple` now.
         """
-        return self._seconds, self._nanoseconds, self._machine_id
-
-    def __str__(self):
-        """ String representation matching SPL's representation.
-        """
-        return str(self.tuple())
+        return self
 
 def _get_timestamp_tuple(ts):
     """
@@ -113,8 +148,10 @@ def _get_timestamp_tuple(ts):
     Handles input being a datetime or a Timestamp.
     """
     if isinstance(ts, datetime.datetime):    
-        return Timestamp.from_datetime().tuple()
-    return ts.tuple()
+        return Timestamp.from_datetime(ts).tuple()
+    elif isinstance(ts, Timestamp):    
+        return ts
+    raise ValueError('Timestamp or dtate.datetime required')
     
 from streamsx.spl.op import Expression
 
