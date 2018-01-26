2b72a244c690be1af9247bc86bf42585aded3aa4
Dan Debrunner <debrunne@us.ibm.com>
2018-01-26 10:40:29 -0800

    Fix #1446 - Timestamp __new__ invocation that works on 2.7/3.5

--- com.ibm.streamsx.topology/opt/python/packages/streamsx/spl/types.py
+++ com.ibm.streamsx.topology/opt/python/packages/streamsx/spl/types.py
@@ -20,7 +20,8 @@ import streamsx.spl.op
 def _stored_ts(s, ns, mid):
     return Timestamp(s, ns, mid)
 
-class Timestamp(collections.namedtuple('Timestamp', ['seconds', 'nanoseconds', 'machine_id'])):
+_Timestamp = collections.namedtuple('Timestamp', ['seconds', 'nanoseconds', 'machine_id'])
+class Timestamp(_Timestamp):
     """
     SPL native timestamp type with nanosecond resolution.
 
@@ -114,7 +115,7 @@ class Timestamp(collections.namedtuple('Timestamp', ['seconds', 'nanoseconds', '
          return ns
 
     def __new__(cls, seconds, nanoseconds, machine_id=0):
-        return super().__new__(cls, int(seconds), Timestamp._check_nanos(nanoseconds), int(machine_id))
+        return _Timestamp.__new__(cls, int(seconds), Timestamp._check_nanos(nanoseconds), int(machine_id))
 
     def time(self):
         """
