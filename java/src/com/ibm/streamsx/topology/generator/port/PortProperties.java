ab376f68c409ae74698ebfd0e496f049670abea9
Will Marshall <wmarshall484@gmail.com>
2018-02-01 14:06:02 -0800

    Create PortProperties interface, and add setParallel to TStream.

---
+++ java/src/com/ibm/streamsx/topology/generator/port/PortProperties.java
@@ -0,0 +1,33 @@
+package com.ibm.streamsx.topology.generator.port;
+
+public interface PortProperties {
+	/**
+	 * The width of the parallel region, if the port is the start of a parallel region.
+	 */
+	String WIDTH = "width";
+	
+	/**
+	 * Boolean top-level parameter indicating whether the port should broadcast tuples
+	 * to all channels in a parallel region, if the port is the start of a parallel
+	 * region.	
+	 */
+	String BROADCAST = "broadcast";
+	
+	/** 
+	 * Top-level boolean parameter indicating whether the port is the start of a 
+	 * parallel region.
+	 */
+	String PARALLEL = "parallel";
+	
+	/**
+	 * Top-level boolean parameter indicating that the downstream parallel region
+	 * is to be partitioned on the tuples of this port.
+	 */
+	String PARTITIONED = "partitioned";
+	
+	/**
+	 * Top-level parameter indicating a list of keys on which the downstream
+	 * parallel region is to be partitioned.
+	 */
+	String PARTITION_KEYS = "partitionedKeys";
+}
