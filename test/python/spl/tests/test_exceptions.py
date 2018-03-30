9ba944ef3aba9c7dfb80c354b3a8bcf32998d8af
Dan Debrunner <debrunne@us.ibm.com>
2018-03-30 15:14:35 -0700

    Handle exceptions in Filter correctly

---
+++ test/python/spl/tests/test_exceptions.py
@@ -0,0 +1,83 @@
+# coding=utf-8
+# Licensed Materials - Property of IBM
+# Copyright IBM Corp. 2016
+from __future__ import print_function
+import unittest
+import sys
+import itertools
+import tempfile
+import os
+
+from streamsx.topology.topology import *
+from streamsx.topology.tester import Tester
+
+import streamsx.scripts.extract
+import streamsx.spl.toolkit
+import streamsx.spl.op as op
+
+def _create_tf():
+    with tempfile.NamedTemporaryFile(delete=False) as fp:
+        fp.write("CREATE\n".encode('utf-8'))
+        fp.flush()
+        return fp.name
+
+
+class TestBaseExceptions(unittest.TestCase):
+    """ Test exceptions in callables
+    """
+    @classmethod
+    def setUpClass(cls):
+        """Extract Python operators in toolkit"""
+        streamsx.scripts.extract.main(['-i', '../testtkpy', '--make-toolkit'])
+
+    def setUp(self):
+        self.tf = _create_tf()
+        Tester.setup_standalone(self)
+
+    def tearDown(self):
+        if self.tf:
+            os.remove(self.tf)
+
+    def _result(self, n):
+        with open(self.tf) as fp:
+            content = fp.readlines()
+        self.assertTrue(len(content) >=3)
+        self.assertEqual('CREATE\n', content[0])
+        self.assertEqual('__init__\n', content[1])
+        self.assertEqual('__enter__\n', content[2])
+        self.assertEqual(n, len(content), msg=str(content))
+        return content
+
+class TestExceptions(TestBaseExceptions):
+
+    def _run_app(self, kind):
+        schema = 'tuple<rstring a, int32 b>'
+        topo = Topology()
+        streamsx.spl.toolkit.add_toolkit(topo, '../testtkpy')
+        s = topo.source(range(234))
+        data = [1,2,3]
+        se = topo.source(data)
+
+        se = se.map(lambda x : {'a':'hello', 'b':x} , schema=schema)
+
+        prim = op.Map(
+            "com.ibm.streamsx.topology.pytest.pyexceptions::" + kind,
+            se, params={'tf':self.tf})
+
+        res = prim.stream
+
+        tester = Tester(topo)
+        tester.tuple_count(s, 234)
+        tester.tuple_count(res, 0)
+        ok = tester.test(self.test_ctxtype, self.test_config, assert_on_fail=False)
+        self.assertFalse(ok)
+
+    def test_exc_enter_filter(self):
+        self._run_app('ExcEnterFilter')
+        self._result(3)
+
+    def test_exc_call_filter(self):
+        self._run_app('ExcCallFilter')
+        content = self._result(5)
+        self.assertEqual('__exit__\n', content[3])
+        self.assertEqual('KeyError\n', content[4])
---
+++ test/python/spl/testtkpy/opt/python/streams/exception.py
@@ -0,0 +1,61 @@
+# coding=utf-8
+# Licensed Materials - Property of IBM
+# Copyright IBM Corp. 2016
+from __future__ import print_function
+import unittest
+import sys
+import itertools
+import tempfile
+import os
+
+# Import the SPL decorators
+from streamsx.spl import spl
+import sys
+
+def spl_namespace():
+    return "com.ibm.streamsx.topology.pytest.pyexceptions"
+
+def _create_tf():
+    with tempfile.NamedTemporaryFile(delete=False) as fp:
+        fp.write("CREATE\n".encode('utf-8'))
+        fp.flush()
+        return fp.name
+
+
+class EnterExit(object):
+    def __init__(self, tf):
+        self.tf = tf
+        self._report('__init__')
+    def __enter__(self):
+        self._report('__enter__')
+    def __exit__(self, exc_type, exc_value, traceback):
+        self._report('__exit__')
+        if exc_type:
+            self._report(exc_type.__name__)
+    def __call__(self, *t):
+        return t
+    def _report(self, txt):
+        with open(self.tf, 'a') as fp:
+            fp.write(txt)
+            fp.write('\n')
+            fp.flush()
+
+class ExcOnEnter(EnterExit):
+    def __enter__(self):
+        super(ExcOnEnter,self).__enter__()
+        raise ValueError('__enter__ has failed!')
+
+class ExcOnCall(EnterExit):
+    def __call__(self, *t):
+        d = {}
+        return d['notthere']
+
+
+@spl.filter()
+class ExcEnterFilter(ExcOnEnter):
+    pass
+
+
+@spl.filter()
+class ExcCallFilter(ExcOnCall):
+    pass
