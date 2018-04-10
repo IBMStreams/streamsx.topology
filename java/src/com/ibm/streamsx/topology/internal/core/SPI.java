f366a440331bd0962bc689bfae7c145d45ce2a41
Dan Debrunner <debrunne@us.ibm.com>
2018-04-10 16:16:18 -0700

    Allow namespace of operators to be set for a SPI user

---
+++ java/src/com/ibm/streamsx/topology/internal/core/SPI.java
@@ -0,0 +1,33 @@
+/*
+# Licensed Materials - Property of IBM
+# Copyright IBM Corp. 2018
+ */
+package com.ibm.streamsx.topology.internal.core;
+
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * Functions/information for the SPI.
+ *
+ */
+public class SPI {
+    
+    private static final Properties SPI_PROPERTIES = new Properties();
+    
+    static {
+        try {
+            SPI_PROPERTIES.load(SPI.class.getResourceAsStream("/com/ibm/streamsx/topology/spi.properties"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    
+    /**
+     * Namespace for Java functional topology operators used by the SPI.
+     * @return
+     */
+    public static String namespaceJava() {
+        return SPI_PROPERTIES.getProperty("namespace.topology.java");
+    }
+}
