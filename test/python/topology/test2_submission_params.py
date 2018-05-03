b6f54bdecee7da2ac1ce45d9e6110d1d57c09441
Dan Debrunner <debrunne@us.ibm.com>
2018-05-03 16:48:50 -0700

    Submission time value working as an output clause

--- test/python/topology/test2_submission_params.py
+++ test/python/topology/test2_submission_params.py
@@ -30,27 +30,35 @@ class TestSubmissionParams(unittest.TestCase):
         Test passing as an SPL parameter.
         """
         N=22
+        G='hey'
         t = ''.join(random.choice('0123456789abcdef') for x in range(20))
         topic = 'topology/test/python/' + t
        
         topo = Topology()
         spTopic = topo.create_submission_parameter('mytopic')
+        spGreet = topo.create_submission_parameter('greeting')
 
         sch = StreamSchema('tuple<uint64 seq, rstring s>')
         b = op.Source(topo, "spl.utility::Beacon", sch,
             params = {'initDelay': 10.0, 'period': 0.02, 'iterations':N})
         b.seq = b.output('IterationCount()')
+        b.s = b.output(spGreet)
      
         p = op.Sink("com.ibm.streamsx.topology.topic::Publish", b.stream,
             params={'topic': topic})
 
         s = op.Source(topo, "com.ibm.streamsx.topology.topic::Subscribe", sch,
             params = {'streamType': sch, 'topic': spTopic})
+        s.stream.print()
 
         jc = JobConfig()
         jc.submission_parameters['mytopic'] = topic
+        jc.submission_parameters['greeting'] = G
         jc.add(self.test_config)
+        self.test_config['topology.keepArtifacts'] = True
 
         tester = Tester(topo)
         tester.tuple_count(s.stream, N)
+        #tester.run_for(300)
+        tester.contents(s.stream, [{'seq':i, 's':G} for i in range(N)])
         tester.test(self.test_ctxtype, self.test_config)
