d363c211bfa29f0528fb416a1a10eb070f2323b4
shenli <shenli@us.ibm.com>
2018-06-25 11:19:13 -0400

    Relocate HashAdder to the front of unparallel for a special case where unparallel has only one child (HashAdder) and parallel has only one parent (HashAdder)

--- java/src/com/ibm/streamsx/topology/generator/spl/Preprocessor.java
+++ java/src/com/ibm/streamsx/topology/generator/spl/Preprocessor.java
@@ -4,11 +4,16 @@
  */
 package com.ibm.streamsx.topology.generator.spl;
 
+import static com.ibm.streamsx.topology.internal.gson.GsonUtilities.jstring;
+
 import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import com.google.gson.JsonObject;
 import com.ibm.streamsx.topology.builder.BVirtualMarker;
+import com.ibm.streamsx.topology.function.Consumer;
 
 /**
  * Preprocessor modifies the passed in JSON to perform
@@ -31,15 +36,13 @@ class Preprocessor {
         GraphValidation graphValidationProcess = new GraphValidation();
         graphValidationProcess.validateGraph(graph);
 
-        // The hash adder operators need to be relocated to enable directly 
-	// adjacent parallel regions
-        // TODO: renable adjacent parallel regions optimization
-        //relocateHashAdders();
+        // The hash adder operators need to be relocated to enable directly
+        // adjacent parallel regions
+        relocateHashAdders();
         
         pePlacementPreprocess.tagIsolationRegions();
         pePlacementPreprocess.tagLowLatencyRegions();
-        
-        
+
         
         ThreadingModel.preProcessThreadedPorts(graph);
         
@@ -67,5 +70,46 @@ class Preprocessor {
             return;
         for (JsonObject composite : composites)
             pePlacementPreprocess.compositeColocateIdUse(composite);
+    }
+
+    @SuppressWarnings("serial")
+    private void relocateHashAdders(){
+        final Set<JsonObject> hashAdders = new HashSet<>();
+        // Firstly, find each hashAdder
+        GraphUtilities.visitOnce(GraphUtilities.findStarts(graph),
+                new HashSet<BVirtualMarker>(),
+                graph,
+                new Consumer<JsonObject>(){
+                    public void accept(JsonObject op) {
+                        if(jstring(op, "kind").equals("com.ibm.streamsx.topology.functional.java::HashAdder")){
+                            hashAdders.add(op);
+                        }
+                    }
+        });
+
+        for(JsonObject hashAdder : hashAdders){
+            relocateHashAdder(hashAdder);
+        }
+    }
+
+    private void relocateHashAdder(JsonObject hashAdder){
+        // $Unparallel$ -> hashAdder -> $Parallel$ -> hashRemover
+        Set<JsonObject> parents = GraphUtilities.getUpstream(hashAdder, graph);
+        
+        // hashAdder has multiple parents
+        if (parents.size() != 1) return;
+
+        JsonObject parent = parents.iterator().next();
+
+        // hashAdder's parent has multiple children
+        if (GraphUtilities.getDownstream(parent, graph).size() != 1)
+            return;
+
+        // Only optimize the case where $Unparallel has only one child, and
+        // $parallel has only one parent
+        if (jstring(parent, "kind").equals(BVirtualMarker.END_PARALLEL.kind())) {
+            GraphUtilities.removeOperator(hashAdder, graph);
+            GraphUtilities.addBefore(parent, hashAdder, graph);
+        }
     }
 }
