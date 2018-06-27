aac140553b5f2701d7bd7810f566e868e5ce0a0c
Dan Debrunner <debrunne@us.ibm.com>
2018-06-27 10:28:31 -0700

    Don't stop for failed tests

--- ci/test_java_embedded.sh
+++ ci/test_java_embedded.sh
@@ -1,4 +1,4 @@
 . $WORKSPACE/ci/setup
 echo 'Testing Java embedded'
 cd $WORKSPACE/test/java
-ant -Dtopology.test.threads=`grep -c ^processor /proc/cpuinfo` unittest.main
+ant -Dtopology.test.haltonfailure=no -Dtopology.test.threads=`grep -c ^processor /proc/cpuinfo` unittest.main
