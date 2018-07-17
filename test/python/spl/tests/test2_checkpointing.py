db232c8692d6ab59c6e8d8690e175d5b06bb16a1
Eric Johnson <ejohnson@us.ibm.com>
2018-07-17 13:38:06 -0400

    tests with checkpointing enabled for decorated python operators.

--- test/python/spl/tests/test2_checkpointing.py
+++ test/python/spl/tests/test2_checkpointing.py
@@ -9,6 +9,14 @@ import streamsx.spl.toolkit
 
 import spl_tests_utils as stu
 
+# Test checkpointing in python decorated operators.
+# These tests run in a way that should cause checkpoints to be created,
+# but do not actually verify that the checkpoints are created and does
+# not even attempt to restore them.
+
+# There should be at least one test for each type of python decorated
+# operators: source, map, filter, for_each, primitive_operator
+
 class TestCheckpointing(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
@@ -22,13 +30,48 @@ class TestCheckpointing(unittest.TestCase):
     def test_source(self):
         topo = Topology("test")
         topo.checkpoint_period = timedelta(seconds=1)
-        #s = topo.source(TimeCounter(iterations=30, period=0.1))
         streamsx.spl.toolkit.add_toolkit(topo, stu._tk_dir('testtkpy'))
         bop = op.Source(topo, "com.ibm.streamsx.topology.pytest.checkpoint::TimeCounter", schema.StreamSchema('tuple<int32 f>').as_tuple(), params={'iterations':30,'period':0.1})
         s = bop.stream
         tester = Tester(topo)
-        #tester.contents(s, range(0,30))
         tester.tuple_count(s, 30)
+        #tester.contents(s, range(0,30))  # why doesn't this work?
         tester.contents(s, list(zip(range(0,30))))
 
         tester.test(self.test_ctxtype, self.test_config, always_collect_logs=True)
+
+    # Source, filter, and map operators
+    def test_filter_map(self):
+        topo = Topology("test")
+        topo.checkpoint_period = timedelta(seconds=1)
+        streamsx.spl.toolkit.add_toolkit(topo, stu._tk_dir('testtkpy'))
+        timeCounter = op.Source(topo, "com.ibm.streamsx.topology.pytest.checkpoint::TimeCounter", schema.StreamSchema('tuple<int32 f>').as_tuple(), params={'iterations':30,'period':0.1})
+        evenFilter = op.Map("com.ibm.streamsx.topology.pytest.checkpoint::StatefulEvenFilter", timeCounter.stream, None, params={})
+        hpo = op.Map("com.ibm.streamsx.topology.pytest.checkpoint::StatefulHalfPlusOne", evenFilter.stream, None, params={})
+        s = hpo.stream
+        tester = Tester(topo)
+        tester.tuple_count(s, 15)
+        tester.contents(s, list(zip(range(1,16))))
+
+        tester.test(self.test_ctxtype, self.test_config, always_collect_logs=True)
+
+    # source, primitive, and for_each operators
+    def test_primitive_foreach(self):
+        topo = Topology("test")
+        topo.checkpoint_period = timedelta(seconds=1)
+        streamsx.spl.toolkit.add_toolkit(topo, stu._tk_dir('testtkpy'))
+        timeCounter = op.Source(topo, "com.ibm.streamsx.topology.pytest.checkpoint::TimeCounter", schema.StreamSchema('tuple<int32 f>').as_tuple(), params={'iterations':30,'period':0.1})
+        fizzbuzz = op.Map("com.ibm.streamsx.topology.pytest.checkpoint::FizzBuzz", timeCounter.stream, schema.StreamSchema('tuple<int32 f, rstring c>').as_tuple())
+        verify = op.Sink("com.ibm.streamsx.topology.pytest.checkpoint::Verify", fizzbuzz.stream)
+        s = fizzbuzz.stream
+        tester = Tester(topo)
+        tester.tuple_count(s, 30)
+        tester.test(self.test_ctxtype, self.test_config, always_collect_logs=True)
+
+class TestDistributedCheckpointing(TestCheckpointing):
+    def setUp(self):
+        Tester.setup_distributed(self)
+
+class TestSasCheckpointing(TestCheckpointing):
+    def setUp(self):
+        Tester.setup_streaming_analytics(self, force_remote_build=True)
--- test/python/spl/testtkpy/opt/python/streams/test_checkpointing.py
+++ test/python/spl/testtkpy/opt/python/streams/test_checkpointing.py
@@ -8,7 +8,7 @@ def spl_namespace():
     return "com.ibm.streamsx.topology.pytest.checkpoint"
 
 # Class defining a source of integers from 0 to the limit, including 0 but
-# excluding the limit.
+# excluding the limit.  Between each tuple, it sleeps for a period.
 # An instance of this class can be dilled.
 @spl.source()
 class TimeCounter(object):
@@ -38,3 +38,55 @@ class TimeCounter(object):
 
     def next(self):
         return self.__next__()
+
+# Pass only even integers.  This is a class, therefore is checkpointed
+# even though it does not have meaningful state.
+@spl.filter(style='position')
+class StatefulEvenFilter(object):
+    def __init__(self): pass
+    def __call__(self, x):
+        return x % 2 == 0
+
+# Map x -> x / 2 + 1.  Again this is stateful although it does not have
+# meaningful state.
+@spl.map(style='position')
+class StatefulHalfPlusOne(object):
+    def __init__(self): pass
+    def __call__(self, x):
+        return (x / 2 + 1,)
+
+# Receive tuples containing integers, and add an rstring attribute
+# 'fizz', 'buzz', 'fizzbuzz', or '', depending on whether the integer
+# is a multiple of 3, 5, both, or neither.
+@spl.primitive_operator(output_ports=['FIZZBUZZ'])
+class FizzBuzz(spl.PrimitiveOperator):
+    # This must be present, or sc will fail because of missing required
+    # parameters args and kwargs.
+    def __init__(self): pass
+
+    @spl.input_port(style='position')
+    def fizz_buzz(self, value): 
+        classification = ''
+        if (value % 3 == 0):
+            classification = classification + "fizz"
+        if (value % 5 == 0):
+            classification = classification + 'buzz'
+
+        self.submit('FIZZBUZZ', (value, classification))
+
+# Receive a streams of tuples <int32, rstring>.  The rstring value
+# is the fizzbuzz classification of the int32.  This verifies
+# the classification.
+@spl.for_each(style='position')
+class Verify(object):
+    def __init__(self): pass
+    # verify that all receieved tuples have been correctly fizzbuzzed
+    def __call__(self, value, classification):
+        if value % 3 == 0 and value % 5 == 0:
+            assert classification == 'fizzbuzz'
+        elif value % 3 == 0:
+            assert classification == 'fizz'
+        elif value % 5 == 0:
+            assert classification == 'buzz'
+        else:
+            assert classification == ''
