58645328618ab6479df568993a22190d9585366b
Eric Johnson <ejohnson@us.ibm.com>
2018-08-01 14:53:07 -0400

    Fixes to consistent.pyi

--- com.ibm.streamsx.topology/opt/python/packages/streamsx/topology/consistent.pyi
+++ com.ibm.streamsx.topology/opt/python/packages/streamsx/topology/consistent.pyi
@@ -1,10 +1,12 @@
 from typing import Union
+from enum import Enum
+from datetime import timedelta
 
 class ConsistentRegionConfig(object):
     class Trigger(Enum):
         OPERATOR_DRIVEN = 1
         PERIODIC = 2
 
-    def operator_driven(drainTimeout:Union[datetime.timedelta, float, None]=None, resetTimeout:Union[datetime.timedelta, float, None]=None, maxConsecutiveAttempts:Union[int, None]=None) -> 'ConsistentRegionConfig': ...
+    def operator_driven(drainTimeout:Union[timedelta, float, None]=None, resetTimeout:Union[timedelta, float, None]=None, maxConsecutiveAttempts:Union[int, None]=None) -> 'ConsistentRegionConfig': ...
 
-    def periodic(period:Union[datetime.timedelta, float], drainTimeout:Union[datetime.timedelta, float, None]=None, resetTimeout:Union[datetime.timedelta, float, None]=None, maxConsecutiveAttempts:Union[int, None]=None) -> 'ConsistentRegionConfig': ...
+    def periodic(period:Union[timedelta, float], drainTimeout:Union[timedelta, float, None]=None, resetTimeout:Union[timedelta, float, None]=None, maxConsecutiveAttempts:Union[int, None]=None) -> 'ConsistentRegionConfig': ...
