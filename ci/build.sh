0f18d7569c0cae0bba78880199bfc4a8826978ed
Dan Debrunner <ddebrunner@users.noreply.github.com>
2018-08-04 14:40:22 -0700

    Setup Python 3.6 env for build.

--- ci/build.sh
+++ ci/build.sh
@@ -1,5 +1,7 @@
 # Requires $HOME/.ant/lib setup correctly - See README.md
 . ${WORKSPACE:?}/ci/setup
+export PYTHONHOME=${ANACONDA36_HOME:?}
+. ${WORKSPACE}/ci/pysetup
 echo 'Building'
 cd $WORKSPACE
 ant
