011f65b4f42abc4dfb18e27c1adcbd03c46af242
Dan Debrunner <ddebrunner@users.noreply.github.com>
2018-08-04 14:46:26 -0700

    Download JUnit 4.10

--- ci/build.sh
+++ ci/build.sh
@@ -2,6 +2,10 @@
 . ${WORKSPACE:?}/ci/setup
 export PYTHONHOME=${ANACONDA36_HOME:?}
 . ${WORKSPACE}/ci/pysetup
+echo 'Download JUnit 4.10'
+mkdir -p $HOME/.ant/lib
+cd $HOME/.ant/lib
+wget http://central.maven.org/maven2/junit/junit/4.10/junit-4.10.jar
 echo 'Building'
 cd $WORKSPACE
 ant
