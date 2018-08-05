df6e8c4bdc625c4645418cce1e183cf09754ed71
Dan Debrunner <ddebrunner@users.noreply.github.com>
2018-08-05 08:44:05 -0700

    Update build.sh

--- ci/build.sh
+++ ci/build.sh
@@ -1,9 +1,6 @@
 # Requires $HOME/.ant/lib setup correctly - See README.md
 . ${WORKSPACE:?}/ci/setup
 echo 'Download JUnit 4.10'
-mkdir -p $HOME/.ant/lib
-cd $HOME/.ant/lib
-wget http://central.maven.org/maven2/junit/junit/4.10/junit-4.10.jar
 echo 'Building'
 cd $WORKSPACE
 ant
