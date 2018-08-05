e215a5964dd6e48338f00ed9db1357df8e8e0fe3
Dan Debrunner <ddebrunner@users.noreply.github.com>
2018-08-05 14:52:01 -0700

    Update test_java_standalone.sh

--- ci/test_java_standalone.sh
+++ ci/test_java_standalone.sh
@@ -1,4 +1,7 @@
 . ${WORKSPACE:?}/ci/setup
+# Build requires a Python 3.5/6 setup.
+export PYTHONHOME=${ANACONDA36_HOME:?}
+. ${WORKSPACE}/ci/pysetup
 echo 'Testing Java standalone'
 cd $WORKSPACE/test/java
 ant -Dtopology.test.haltonfailure=no -Dtopology.test.threads=${CI_TEST_THREADS:-8} unittest.standalone
