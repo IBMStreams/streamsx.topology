50d23e2662587a74b7cbbdc0a64c31b0da6ca5fb
Eric Johnson <ejohnson@us.ibm.com>
2018-08-08 17:35:12 -0400

    fix unintended format change.

--- com.ibm.streamsx.topology/opt/python/packages/streamsx/topology/topology.py
+++ com.ibm.streamsx.topology/opt/python/packages/streamsx/topology/topology.py
@@ -1001,7 +1001,7 @@ class Stream(_placement._Placement, object):
 
     def transform(self, func, name=None):
         """
-        Equivalent to calling :py:meth:``map(func,name)``.
+        Equivalent to calling :py:meth:``map(func, name)``.
 
         .. deprecated:: 1.7
             Replaced by :py:meth:`map`.
