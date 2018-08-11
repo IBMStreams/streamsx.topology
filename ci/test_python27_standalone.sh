e17145f80622d7fb723ae7e3ca2e906935e3fccd
Dan Debrunner <ddebrunner@users.noreply.github.com>
2018-08-11 16:08:05 -0700

    Fix pip command.

--- ci/test_python27_standalone.sh
+++ ci/test_python27_standalone.sh
@@ -1,6 +1,6 @@
 . ${WORKSPACE:?}/ci/setup
 export PYTHONHOME=${ANACONDA27_HOME:?}
 . ${WORKSPACE}/ci/pysetup
-${PYTHONHOME}/bin/python -m pip funcsigs
+${PYTHONHOME}/bin/python -m pip install funcsigs
 echo 'Testing Python 2.7 standalone' 
 $WORKSPACE/ci/python_standalone.sh
