e479b183d3d5dca612edd1c18a7eec1484f504f4
Dan Debrunner <debrunne@us.ibm.com>
2018-08-16 12:43:10 -0700

    Enable Python 3.5 tests including service tests

--- ci/test_python35_standalone.sh
+++ ci/test_python35_standalone.sh
@@ -1,5 +1,11 @@
 . ${WORKSPACE:?}/ci/setup
+
+# 3.5 environment is assumed to be conda environment created from 3.6
+source ${ANACONDA36_HOME:?}/bin/activate `basename ${ANACONDA35_HOME}`
 export PYTHONHOME=${ANACONDA35_HOME:?}
 . ${WORKSPACE}/ci/pysetup
 echo 'Testing Python 3.5 standalone' 
 $WORKSPACE/ci/python_standalone.sh
+prc=$?
+source ${ANACONDA36_HOME:?}/bin/deactivate `basename ${ANACONDA35_HOME}`
+exit ${prc}
