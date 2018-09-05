5632d9892453d55c943ac0ddf1c37a183d59c2d8
Dan Debrunner <debrunne@us.ibm.com>
2018-09-05 14:22:30 -0700

    Fix super-class

--- test/python/topology/test2_consistent.py
+++ test/python/topology/test2_consistent.py
@@ -1,6 +1,6 @@
 from streamsx.topology.topology import *
 from streamsx.topology.tester import Tester
-from streamsx.topology.tester_runtime import _PythonCondition
+from streamsx.topology.tester_runtime import _FunctionalCondition
 from streamsx.topology.consistent import ConsistentRegionConfig
 import streamsx.spl.op as op
 import streamsx.ec as ec
@@ -98,7 +98,7 @@ class VerifyNumericOrder(object):
 
 # Verify that __enter__ and __exit__ are called, alternately, a specified 
 # number of times.
-class VerifyEnterExit(_PythonCondition):
+class VerifyEnterExit(_FunctionalCondition):
     def __init__(self, expected_minimum, name):
         super(VerifyEnterExit, self).__init__(name)
         self.expected_minimum = expected_minimum
