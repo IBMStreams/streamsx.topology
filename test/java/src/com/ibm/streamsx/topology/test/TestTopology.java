ed0821b8b9a4f8faf79f9746f16134aed94f7e4a
Dan Debrunner <debrunne@us.ibm.com>
2018-09-27 13:16:28 -0700

    Don't add Jacoco flags when topology.test.coverage is false. Fixes #456

--- test/java/src/com/ibm/streamsx/topology/test/TestTopology.java
+++ test/java/src/com/ibm/streamsx/topology/test/TestTopology.java
@@ -76,24 +76,23 @@ public class TestTopology {
     
     @Before
     public void setupConfig() {
-           
-        if (getTesterType() == Type.STANDALONE_TESTER || getTesterType() == Type.DISTRIBUTED_TESTER) {
-                       
-            File agentJar = new File(System.getProperty("user.home"), ".ant/lib/jacocoagent.jar");
-            if (agentJar.exists()) {
-                List<String> vmArgs = new ArrayList<>();
-                String now = Long.toHexString(System.currentTimeMillis());
-                String destFile = "jacoco_" + getTesterType().name() + now + ".exec";
-     
-                
-                String arg = "-javaagent:"
-                        + agentJar.getAbsolutePath()
-                        + "=destfile="
-                        + destFile;
-                vmArgs.add(arg);
-                config.put(ContextProperties.VMARGS, vmArgs);
-            }
-        }
+    	
+		if (Boolean.getBoolean("topology.test.coverage")) {
+
+			if (getTesterType() == Type.STANDALONE_TESTER || getTesterType() == Type.DISTRIBUTED_TESTER) {
+
+				File agentJar = new File(System.getProperty("user.home"), ".ant/lib/jacocoagent.jar");
+				if (agentJar.exists()) {
+					List<String> vmArgs = new ArrayList<>();
+					String now = Long.toHexString(System.currentTimeMillis());
+					String destFile = "jacoco_" + getTesterType().name() + now + ".exec";
+
+					String arg = "-javaagent:" + agentJar.getAbsolutePath() + "=destfile=" + destFile;
+					vmArgs.add(arg);
+					config.put(ContextProperties.VMARGS, vmArgs);
+				}
+			}
+		}
             
         // Look for a different compiler
         String differentCompile = System.getProperty(ContextProperties.COMPILE_INSTALL_DIR);
