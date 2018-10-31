4594a6daf1f39f0cbc1c5175340b0479fa96df62
Dan Debrunner <ddebrunner@users.noreply.github.com>
2018-10-31 14:40:49 -0700

    Pending port name (#1948)
    * Submitjob API changed
    
    * Tie attribute to port alias, not input stream name

--- com.ibm.streamsx.topology/opt/python/packages/streamsx/spl/op.py
+++ com.ibm.streamsx.topology/opt/python/packages/streamsx/spl/op.py
@@ -219,7 +219,8 @@ class Invoke(_placement._Placement, exop.ExtensionOperator):
         if len(self._inputs) == 1:
             return Expression('attribute', name)
         else:
-            return Expression('attribute', stream.oport.name + '.' + name)
+            iport = self._op().inputPorts[self._inputs.index(stream)]
+            return Expression('attribute', iport._alias + '.' + name)
 
     def expression(self, value):
         """SPL expression.
