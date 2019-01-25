448bea58abf5920c5e79ce8c62ac022a821147f2
Dan Debrunner <djd@debrunners.com>
2019-01-25 22:49:16 +0000

    Bump version

--- com.ibm.streamsx.topology/opt/python/packages/streamsx/_streams/_version.py
+++ com.ibm.streamsx.topology/opt/python/packages/streamsx/_streams/_version.py
@@ -3,7 +3,7 @@
 # Copyright IBM Corp. 2018
 
 ## Common PEP396 version for modules shipped with streamsx packages
-__version__='1.12.0dev1'
+__version__='1.12.0dev5'
 
 import pkg_resources
 import sys
