b4b332b33554a27abbd3b8f88eb1a1497550a8be
Dan Debrunner <djd@debrunners.com>
2019-01-25 18:32:38 +0000

    Dev release for build service

--- com.ibm.streamsx.topology/opt/python/packages/streamsx/_streams/_version.py
+++ com.ibm.streamsx.topology/opt/python/packages/streamsx/_streams/_version.py
@@ -3,7 +3,7 @@
 # Copyright IBM Corp. 2018
 
 ## Common PEP396 version for modules shipped with streamsx packages
-__version__='1.12.0dev'
+__version__='1.12.0dev1'
 
 import pkg_resources
 import sys
