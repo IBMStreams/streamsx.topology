64b60819d345eb5693531d43ba6d26db00900ad1
Dan Debrunner <djd@debrunners.com>
2019-05-03 21:33:42 +0000

    Add generic forwarder between contexts

--- java/src/com/ibm/streamsx/topology/internal/context/service/RemoteStreamingAnalyticsServiceStreamsContext.java
+++ java/src/com/ibm/streamsx/topology/internal/context/service/RemoteStreamingAnalyticsServiceStreamsContext.java
@@ -4,20 +4,10 @@
  */
 package com.ibm.streamsx.topology.internal.context.service;
 
-import static com.ibm.streamsx.topology.internal.gson.GsonUtilities.jstring;
-import static com.ibm.streamsx.topology.internal.gson.GsonUtilities.object;
-
-import java.math.BigInteger;
-import java.util.concurrent.Future;
-
-import com.google.gson.JsonObject;
+import com.ibm.streamsx.rest.StreamingAnalyticsService;
 import com.ibm.streamsx.topology.context.StreamsContext;
-import com.ibm.streamsx.topology.context.remote.RemoteContext;
-import com.ibm.streamsx.topology.internal.context.JSONStreamsContext;
+import com.ibm.streamsx.topology.internal.context.RemoteContextForwarderStreamsContext;
 import com.ibm.streamsx.topology.internal.context.remote.RemoteBuildAndSubmitRemoteContext;
-import com.ibm.streamsx.topology.internal.context.remote.SubmissionResultsKeys;
-import com.ibm.streamsx.topology.internal.process.CompletedFuture;
-import com.ibm.streamsx.topology.internal.messages.Messages;
 
 /**
  * Context that submits the SPL to the Streaming Analytics service
@@ -25,30 +15,14 @@ import com.ibm.streamsx.topology.internal.messages.Messages;
  * 
  * This delegates to the true remote context: RemoteBuildAndSubmitRemoteContext.
  */
-public class RemoteStreamingAnalyticsServiceStreamsContext extends JSONStreamsContext<BigInteger> {
-    
-    private final RemoteBuildAndSubmitRemoteContext remoteContext;
-    
+public class RemoteStreamingAnalyticsServiceStreamsContext extends RemoteContextForwarderStreamsContext<StreamingAnalyticsService> {
+        
     public RemoteStreamingAnalyticsServiceStreamsContext() {
-        remoteContext = new RemoteBuildAndSubmitRemoteContext();
+        super(new RemoteBuildAndSubmitRemoteContext());
     }
 
     @Override
     public com.ibm.streamsx.topology.context.StreamsContext.Type getType() {
         return StreamsContext.Type.STREAMING_ANALYTICS_SERVICE;
-    }
-
-    @Override
-    protected Future<BigInteger> action(com.ibm.streamsx.topology.internal.context.JSONStreamsContext.AppEntity entity) throws Exception {
-        remoteContext.submit(entity.submission).get();
-
-        JsonObject results = object(entity.submission, RemoteContext.SUBMISSION_RESULTS);
-        if (results != null) {
-            String jobId = jstring(results, SubmissionResultsKeys.JOB_ID);
-            if (jobId != null)
-                return new CompletedFuture<>(new BigInteger(jobId));
-        }
-        
-        throw new IllegalStateException(Messages.getString("CONTEXT_JOB_SUBMISSION_FAILED"));
     }
 }
