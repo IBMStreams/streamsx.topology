0097e3cec9d05df0aefb7c14c59b95ea1f6fb552
Eric Johnson <ejohnson@us.ibm.com>
2019-06-27 19:00:53 -0400

    More cleanup

--- com.ibm.streamsx.topology/opt/python/packages/streamsx/rest_primitives.py
+++ com.ibm.streamsx.topology/opt/python/packages/streamsx/rest_primitives.py
@@ -164,7 +164,7 @@ def _handle_http_errors(res):
         #logger.error("Response returned with error code: " + str(res.status_code))
         #logger.error(res.text)
         res.raise_for_status()
-                
+
 
 class _StreamsRestClient(object):
     _blocked_ssl_warn = False
