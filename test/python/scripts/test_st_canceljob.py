7a27e3fc3870b2ba415494491475b5d24ceac02b
Karsten12 <kfonseca@berkeley.edu>
2019-07-05 15:30:14 -0700

    added more tests, fixed bugs in streamtool

--- test/python/scripts/test_st_canceljob.py
+++ test/python/scripts/test_st_canceljob.py
@@ -63,6 +63,19 @@ class TestCancelJob(unittest.TestCase):
                 raise
         self.fail("Job not canceled: " + job.status)
 
+    def _check_job_running(self, job):
+        try:
+            job.refresh()
+            if job.status == "running":
+                return
+            time.sleep(0.2)
+        except requests.exceptions.HTTPError as e:
+            # Just incase we miss the state change.
+            if e.response.status_code == 404:
+                return
+            raise
+        self.fail("Job canceled: " + job.status)
+
     def test_cancel(self):
         job = self._submit_job()
         self._run_canceljob(args=["--jobs", str(job.id)])
@@ -121,6 +134,148 @@ class TestCancelJob(unittest.TestCase):
             ]
         )
 
+    # Check succesfully cancels jobs via --jobnames arg
+    def test_cancel_multiple_2(self):
+        job1 = self._submit_job()
+        job2 = self._submit_job()
+        job3 = self._submit_job()
+
+        self.jobs_to_cancel.extend([job1, job2, job3])
+
+        output, rc = self.get_output(
+            lambda: self._run_canceljob(
+                args=[
+                    "--jobnames",
+                    str(job1.name) + "," + str(job2.name) + "," + str(job3.name),
+                ]
+            )
+        )
+
+        output = output.splitlines()
+
+        self.assertEqual(output[0], self.get_canceljob_output_message(job1.id, 1))
+        self.assertEqual(output[1], self.get_canceljob_output_message(job2.id, 1))
+        self.assertEqual(output[2], self.get_canceljob_output_message(job3.id, 1))
+
+        self._check_job_cancelled(job1)
+        self._check_job_cancelled(job2)
+        self._check_job_cancelled(job3)
+
+        self.assertEqual(rc, 0)
+
+    # Check able to canceljob via jobsIDS from file
+    def test_cancel_multiple_from_file(self):
+        job1 = self._submit_job()
+        job2 = self._submit_job()
+        job3 = self._submit_job()
+
+        self.write_file([job1.id, job2.id, job3.id])
+
+        self.jobs_to_cancel.extend([job1, job2, job3])
+
+        output, rc = self.get_output(
+            lambda: self._run_canceljob(
+                args=["--file", str("test_st_canceljob_tempfile.txt")]
+            )
+        )
+
+        output = output.splitlines()
+
+        self.assertEqual(output[0], self.get_canceljob_output_message(job1.id, 1))
+        self.assertEqual(output[1], self.get_canceljob_output_message(job2.id, 1))
+        self.assertEqual(output[2], self.get_canceljob_output_message(job3.id, 1))
+
+        self._check_job_cancelled(job1)
+        self._check_job_cancelled(job2)
+        self._check_job_cancelled(job3)
+
+        self.assertEqual(rc, 0)
+
+    # Check raises error when given non integer jobID
+    def test_cancel_nonexistant(self):
+        job1 = self._submit_job()
+        job2 = self._submit_job()
+
+        self.jobs_to_cancel.extend([job1, job2])
+
+        # Check invalid/non-numeric ID followed by valid ID - Should print error message regarding invalid one, then do nothing (ie valid one still running)
+        output, rc = self.get_output(
+            lambda: self._run_canceljob(
+                args=["--jobs", str("FAKE_JOB_ID") + "," + str(job1.id)]
+            )
+        )
+
+        self.assertEqual(output, self.get_canceljob_output_message("FAKE_JOB_ID", 5))
+        self.assertEqual(rc, 1)
+        self._check_job_running(job1)
+
+        # Check validID followed by invalid/non-numeric ID - Should cancel valid one w/ success message, then print error message regarding invalid one
+        output, rc = self.get_output(
+            lambda: self._run_canceljob(
+                args=["--jobs", str(job1.id) + "," + str("FAKE_JOB_ID")]
+            )
+        )
+
+        output = output.splitlines()
+
+        self.assertEqual(output[0], self.get_canceljob_output_message(job1.id, 1))
+        self.assertEqual(output[1], self.get_canceljob_output_message("FAKE_JOB_ID", 5))
+        self.assertEqual(rc, 1)
+        self._check_job_cancelled(job1)
+
+        # Try cancelling invalid/numeric ID followed by valid id - Should print 2 error messages regarding invalid, then cancel valid one and print success message
+        output, rc = self.get_output(
+            lambda: self._run_canceljob(
+                args=["--jobs", str("123456") + "," + str(job2.id)]
+            )
+        )
+
+        output = output.splitlines()
+
+        self.assertEqual(output[0], self.get_canceljob_output_message("123456", 3))
+        self.assertEqual(output[1], self.get_canceljob_output_message("123456", 4))
+        self.assertEqual(output[2], self.get_canceljob_output_message(job2.id, 1))
+        self.assertEqual(rc, 0)
+        self._check_job_cancelled(job2)
+
+    # Check invalid jobname error message, then check valid jobname cancellation message
+    def test_cancel_nonexistant2(self):
+        job1 = self._submit_job()
+
+        self.jobs_to_cancel.extend([job1])
+
+        output, rc = self.get_output(
+            lambda: self._run_canceljob(
+                args=["--jobnames", str("FAKE_JOB_NAME") + "," + str(job1.name)]
+            )
+        )
+
+        output = output.splitlines()
+
+        # Should give error regarding invalid jobname, then cancel valid jobname
+        self.assertEqual(
+            output[0], self.get_canceljob_output_message("FAKE_JOB_NAME", 2)
+        )
+        self.assertEqual(output[1], self.get_canceljob_output_message(job1.id, 1))
+
+        self.assertEqual(rc, 0)
+        self._check_job_cancelled(job1)
+
+    def setUp(self):
+        self.instance = os.environ["STREAMS_INSTANCE_ID"]
+        self.username = os.environ["STREAMS_USERNAME"]
+        self.jobs_to_cancel = []
+
+    def tearDown(self):
+        for job in self.jobs_to_cancel:
+            job.cancel(force=True)
+        if os.path.exists("test_st_canceljob_tempfile.txt"):
+            os.remove("test_st_canceljob_tempfile.txt")
+
+    ###########################################
+    # Helper functions
+    ###########################################
+
     def write_file(self, jobIDs):
         with open("test_st_canceljob_tempfile.txt", "w") as f:
             for jobID in jobIDs:
@@ -138,20 +293,11 @@ class TestCancelJob(unittest.TestCase):
             4: "The following job ID cannot be canceled: {}. See the previous error message".format(
                 job_data
             ),
+            5: "The following job identifier is not valid: {}. Specify a job identifier that is numeric and try the request again.".format(
+                job_data
+            ),
         }
         return switch[returnMessage]
-
-    def setUp(self):
-        self.instance = os.environ["STREAMS_INSTANCE_ID"]
-        self.stringLength = 10
-        self.username = os.environ["STREAMS_USERNAME"]
-        self.jobs_to_cancel = []
-
-    def tearDown(self):
-        for job in self.jobs_to_cancel:
-            job.cancel(force=True)
-        if os.path.exists("test_st_canceljob_tempfile.txt"):
-            os.remove("test_st_canceljob_tempfile.txt")
 
     def get_output(self, my_function):
         """ Helper function that gets the ouput from executing my_function
