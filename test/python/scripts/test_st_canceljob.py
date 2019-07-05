0f0b3b02cff6ae856e0a8e259f8fa7202cef6819
Karsten12 <kfonseca@berkeley.edu>
2019-07-04 23:15:06 -0700

    added 1 test, passes

--- test/python/scripts/test_st_canceljob.py
+++ test/python/scripts/test_st_canceljob.py
@@ -11,31 +11,49 @@ from streamsx.topology.topology import Topology
 from streamsx.topology.context import submit, ConfigParams
 import streamsx.scripts.streamtool as streamtool
 
-# Tests streamtool canceljob script.
-#
-# Requires environment setup for a ICP4D Streams instance.
+from contextlib import contextmanager
+from io import StringIO
 
-@unittest.skipUnless('ICP4D_DEPLOYMENT_URL' in os.environ and 'STREAMS_INSTANCE_ID' in os.environ and 'STREAMS_USERNAME' in os.environ and 'STREAMS_PASSWORD' in os.environ , "requires Streams REST API setup")
+# Tests streamtool canceljob script.
+# Requires environment setup for a ICP4D Streams instance.
+@contextmanager
+def captured_output():
+    new_out, new_err = StringIO(), StringIO()
+    old_out, old_err = sys.stdout, sys.stderr
+    try:
+        sys.stdout, sys.stderr = new_out, new_err
+        yield sys.stdout, sys.stderr
+    finally:
+        sys.stdout, sys.stderr = old_out, old_err
+
+
+@unittest.skipUnless(
+    "ICP4D_DEPLOYMENT_URL" in os.environ
+    and "STREAMS_INSTANCE_ID" in os.environ
+    and "STREAMS_USERNAME" in os.environ
+    and "STREAMS_PASSWORD" in os.environ,
+    "requires Streams REST API setup",
+)
 class TestCancelJob(unittest.TestCase):
     def _submit_job(self):
         topo = Topology()
-        topo.source(['Hello', 'World']).print()
+        topo.source(["Hello", "World"]).print()
         cfg = {}
         cfg[ConfigParams.FORCE_REMOTE_BUILD] = True
         cfg[ConfigParams.SSL_VERIFY] = False
-        sr = submit('DISTRIBUTED', topo, cfg)
+        sr = submit("DISTRIBUTED", topo, cfg)
         return sr.job
 
     def _run_canceljob(self, args):
-        args.insert(0, '--disable-ssl-verify')
-        args.insert(1, 'canceljob')
-        streamtool.main(args=args)
+        args.insert(0, "--disable-ssl-verify")
+        args.insert(1, "canceljob")
+        return streamtool.main(args=args)
 
     def _check_job_cancelled(self, job):
         for _ in range(100):
             try:
                 job.refresh()
-                if job.status == 'canceling':
+                if job.status == "canceling":
                     return
                 time.sleep(0.2)
             except requests.exceptions.HTTPError as e:
@@ -43,24 +61,98 @@ class TestCancelJob(unittest.TestCase):
                 if e.response.status_code == 404:
                     return
                 raise
-        self.fail('Job not canceled: ' + job.status)
+        self.fail("Job not canceled: " + job.status)
 
     def test_cancel(self):
         job = self._submit_job()
-        self._run_canceljob(args=['--jobs', str(job.id)])
+        self._run_canceljob(args=["--jobs", str(job.id)])
 
     def test_cancel_user(self):
         job = self._submit_job()
-        user = os.environ['STREAMS_USERNAME']
-        with unittest.mock.patch.dict(os.environ, {'STREAMS_USERNAME':''}):
-            self._run_canceljob(args=['--jobs', str(job.id), '--User', user])
+        user = os.environ["STREAMS_USERNAME"]
+        with unittest.mock.patch.dict(os.environ, {"STREAMS_USERNAME": ""}):
+            self._run_canceljob(args=["--jobs", str(job.id), "--User", user])
         self._check_job_cancelled(job)
 
     def test_cancel_multiple(self):
         job1 = self._submit_job()
         job2 = self._submit_job()
         job3 = self._submit_job()
-        self._run_canceljob(args=['--jobs', str(job1.id)+','+str(job2.id)+','+str(job3.id)])
+        self._run_canceljob(
+            args=["--jobs", str(job1.id) + "," + str(job2.id) + "," + str(job3.id)]
+        )
         self._check_job_cancelled(job1)
         self._check_job_cancelled(job2)
         self._check_job_cancelled(job3)
+
+    def test_cancel_multiple_mix(self):
+        job1 = self._submit_job()
+        job2 = self._submit_job()
+        job3 = self._submit_job()
+        job4 = self._submit_job()
+        self.jobs_to_cancel.extend([job1, job2, job3, job4])
+
+        output, rc = self.get_output(
+            lambda: self._run_canceljob(
+                args=[
+                    "--jobs",
+                    str(job1.id) + "," + str(job2.id),
+                    "--jobnames",
+                    str(job3.name) + "," + str(job4.name),
+                ]
+            )
+        )
+
+        output = output.splitlines()
+
+        self.assertEqual(output[0], self.get_output_message(job1, 1))
+        self.assertEqual(output[1], self.get_output_message(job2, 1))
+        self.assertEqual(output[2], self.get_output_message(job3, 1))
+        self.assertEqual(output[3], self.get_output_message(job4, 1))
+
+        self._check_job_cancelled(job1)
+        self._check_job_cancelled(job2)
+        self._check_job_cancelled(job3)
+        self._check_job_cancelled(job4)
+        self.assertEqual(rc, 0)
+
+    def get_output_message(self, job, returnMessage):
+        switch = {
+            1: "The following job ID was canncelled: {}. The job was in the {} instance.".format(
+                job.id, self.instance
+            ),
+            2: "The following job name is not found: {}. Specify a job name that is valid and try the request again".format(
+                job.name
+            ),
+            3: "The following job ID was not found {}".format(job.id),
+            4: "The following job ID cannot be canncelled: {}. See the previous error message".format(
+                job.id
+            ),
+        }
+        return switch[returnMessage]
+
+    def setUp(self):
+        self.instance = os.environ["STREAMS_INSTANCE_ID"]
+        self.stringLength = 10
+        self.username = os.environ["STREAMS_USERNAME"]
+        self.jobs_to_cancel = []
+
+    def tearDown(self):
+        for job in self.jobs_to_cancel:
+            job.cancel(force=True)
+
+    def get_output(self, my_function):
+        """ Helper function that gets the ouput from executing my_function
+
+        Arguments:
+            my_function {} -- The function to be executed
+
+        Returns:
+            Output [String] -- Output of my_function
+            Rc [int] -- 0 indicates succces, 1 indicates error or failure
+        """
+        rc = None
+        with captured_output() as (out, err):
+            rc = my_function()
+        output = out.getvalue().strip()
+        return output, rc
