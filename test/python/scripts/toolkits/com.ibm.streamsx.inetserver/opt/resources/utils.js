638a448c3cf714107491f96edf3bfde10ebddef8
Karsten12 <kfonseca@berkeley.edu>
2019-08-06 10:41:22 -0700

    added files for 2 tests

--- test/python/scripts/toolkits/com.ibm.streamsx.inetserver/opt/resources/utils.js
+++
@@ -1,7 +0,0 @@
-localTimeString = function(seconds) { return new Date(seconds*1000).toLocaleTimeString(); };
-localTimestampString = function(seconds) { return new Date(seconds*1000).toLocaleString(); };
-localTimeMillisString = function(ms) { return new Date(ms).toLocaleString(); };
-
-fmtLink = function(path) {
-  return '<a href="' + path + '">' + path + '</a>';
-}
