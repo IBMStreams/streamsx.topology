619d53e71ade0546ea0b07d7dba7976948a56814
Dan Debrunner <djd@debrunners.com>
2019-10-03 17:08:27 +0000

    1.13.10 release

--- com.ibm.streamsx.topology/opt/python/packages/streamsx/_streams/_version.py
+++ com.ibm.streamsx.topology/opt/python/packages/streamsx/_streams/_version.py
@@ -3,7 +3,7 @@
 # Copyright IBM Corp. 2018,2019
 
 ## Common PEP396 version for modules shipped with streamsx packages
-__version__='1.13.9'
+__version__='1.13.10'
 
 import pkg_resources
 import sys
