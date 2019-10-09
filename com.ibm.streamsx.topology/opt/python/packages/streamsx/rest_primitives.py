9879d62cf23478e601688e1f6b7437c46bbcf687
Dan Debrunner <djd@debrunners.com>
2019-10-09 22:00:25 +0000

    Add job groups object and PE.get_resource

--- com.ibm.streamsx.topology/opt/python/packages/streamsx/rest_primitives.py
+++ com.ibm.streamsx.topology/opt/python/packages/streamsx/rest_primitives.py
@@ -879,7 +879,7 @@ class Job(_ResourceElement):
         health (str): Health indicator for the job. Some possible values for this property include *healthy*,
             *partiallyHealthy*, *partiallyUnhealthy*, *unhealthy*, and *unknown*.
         applicationName (str): Name of the streams processing application that this job is running.
-        jobGroup (str): Identifies the job group to which this job belongs.
+        jobGroup (str): Streams 4.2/4.3 only. Identifies the job group to which this job belongs.
         startedBy (str): Identifies the user ID that started this job.
         status (str): Status of this job. Some possible values for this property include *canceling*, *running*,
             *canceled*, and *unknown*.
@@ -1031,6 +1031,26 @@ class Job(_ResourceElement):
         """
         return self.rest_client._sc._delegator._cancel_job(self, force)
 
+    def get_job_group(self):
+        """
+        Get the job group associated with this job.
+
+        .. versionadded:: 1.13.13
+        """
+        # 4.x returned just the name for jobGroup.
+        if self.jobGroup.startswith('https://'):
+            return JobGroup(self.rest_client.make_request(self.jobGroup), self.rest_client)
+        return JobGroup({'name':self.jobGroup}, self.rest_client)
+
+class JobGroup(_ResourceElement):
+    """A job group definition.
+
+        Attributes:
+        name(str): Job group name.
+        resourceType(str): Identifies the REST resource type, which is *jobGroup*.
+
+        .. versionadded:: 1.13.13
+    """
 
 class Operator(_ResourceElement):
     """An operator invocation within a job.
@@ -1296,6 +1316,17 @@ class PE(_ResourceElement):
         """
         if hasattr(self, 'host') and self.host:
             return Host(self.rest_client.make_request(self.host), self.rest_client)
+
+    def get_resource(self):
+        """Get resource this processing element is currently executing in.
+
+        Returns:
+            Host: Resource this processing element is running on.
+
+        .. versionadded:: 1.13.13
+        """
+        if hasattr(self, 'resource') and self.resource:
+            return Resource(self.rest_client.make_request(self.resource), self.rest_client)
 
     def retrieve_trace(self, filename=None, dir=None):
         """Retrieves the application trace files for this PE
@@ -1904,6 +1935,23 @@ class Instance(_ResourceElement):
             >>> jobs = instance.get_jobs(name="*temperatureApplication*")
         """
         return self._get_elements(self.jobs, 'jobs', Job, None, name)
+
+    def get_job_groups(self, name=None):
+        """Retrieves job groups defined in this instance.
+
+        Args:
+            name (str, optional): Only return job groups containing property **name** that matches `name`. `name` can be a
+                regular expression. If `name` is not supplied, then all job groups are returned.
+
+        Returns:
+            list(JobGroup): A list of job groups matching the given `name`.
+
+        Only supported for Streams 5.0 and later.
+
+        .. versionadded:: 1.13.13
+        """
+        if hasattr(self, 'jobGroups'):
+            return self._get_elements(self.jobGroups, 'jobGroups', JobGroup, None, name)
 
     def get_job(self, id):
         """Retrieves a job matching the given `id`
