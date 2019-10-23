3b6807b68c551f28b6e0d9491acc6a34a25451fc
Karsten12 <kfonseca@berkeley.edu>
2019-10-23 14:43:19 -0700

    updated tests for streamsx-sc to fix errors when multiple users run tests, still need to test

--- test/python/scripts/toolkits/create_test_sc_files.sh
+++ test/python/scripts/toolkits/create_test_sc_files.sh
@@ -9,19 +9,52 @@ PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 # Use the same random name for each toolkit.
 NAME=$(head -c16 <(tr -dc '[:lower:]' < /dev/urandom 2>/dev/null))
-echo "Random toolkit name is com.example.${NAME}."
+NAME="com.example.${NAME}"
+echo "Random toolkit name is ${NAME}"
 
 # Create info.xml files for the toolkits located in /scripts/toolkits/
 for i in $(find ${PARENT_PATH} -name info.xml.tmpl); do
-    sed -e "s/com.example./com.example.${NAME}./" "$i" > "${i/.tmpl/}"
+    sed -e "s/com.example./${NAME}./" "$i" > "${i/.tmpl/}"
+done
+
+# Creat the toolkit spl folder w/ the random NAME from the template folder
+# ie convert tmp.test_tk_1.test1 -> com.example.mcgmufaipldqwcyo.test_tk_1.test1
+for i in $(find ${PARENT_PATH} -type d -name 'tmp.test_tk_*.test1'); do
+    NEWDIR=${i/tmp/$NAME}
+    # echo $NEWDIR
+    cp -R $i $NEWDIR
+done
+
+# Create the toolkit spl file w/ the random NAME from the template file
+# ie create toolkit.spl from toolkit.spl.tmpl
+NEWDIR_PATH="${PARENT_PATH}/*/com.example.*.test_tk_*.test1"
+for i in $(find $NEWDIR_PATH -name 'toolkit.spl.tmpl'); do
+    sed -e "s/test_tk/${NAME}.test_tk/" "$i" > "${i/.tmpl/}"
+    rm $i
 done
 
 # Create toolkit.xml files for the toolkits located in /scripts/toolkits/
 for i in $(find ${PARENT_PATH} -name toolkit.xml.tmpl); do
-    sed -e "s/com.example./com.example.${NAME}./" "$i" > "${i/.tmpl/}"
+    sed -e "s/test_tk_/${NAME}.test_tk_/" "$i" > "${i/.tmpl/}"
+done
+
+# Create the samplemain spl folder w/ the random NAME from the template folder, located in /scripts/apps/
+# ie create samplemain from tmp.samplemain
+for i in $(find ${PARENT_PATH}/../apps/ -type d -name 'tmp.samplemain'); do
+    NEWDIR=${i/tmp./}
+    cp -R $i $NEWDIR
+done
+
+# Create the test.spl file w/ the random NAME from the template file, located in /scripts/apps/
+# ie create test.spl from test.spl.tmpl
+NEWDIR_PATH2="${PARENT_PATH}/../apps/*/samplemain"
+# Create toolkit.xml files for the toolkits located in /scripts/toolkits/
+for i in $(find $NEWDIR_PATH2 -name test.spl.tmpl); do
+    sed -e "s/test_tk/${NAME}.test_tk/" "$i" > "${i/.tmpl/}"
+    rm $i
 done
 
 # Create info.xml files for the apps, located in /scripts/apps/
 for i in $(find ${PARENT_PATH}/../apps/ -name info.xml.tmpl); do
-    sed -e "s/com.example./com.example.${NAME}./" "$i" > "${i/.tmpl/}"
+    sed -e "s/com.example./${NAME}./" "$i" > "${i/.tmpl/}"
 done
--- test/python/scripts/toolkits/delete_test_sc_files.sh
+++ test/python/scripts/toolkits/delete_test_sc_files.sh
@@ -9,8 +9,14 @@ PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 # Delete info.xml files for the toolkits located in /scripts/toolkits/
 find ${PARENT_PATH} -name info.xml | xargs rm
 
+# Delete generated com.example.*.test_tk_*.test1 folder
+find $PARENT_PATH -type d -name 'com.example.*.test_tk_*.test1' | xargs rm -r
+
 # Delete toolkit.xml files for the toolkits located in /scripts/toolkits/
 find ${PARENT_PATH} -name toolkit.xml | xargs rm
+
+# Delete generated samplemain folder
+find ${PARENT_PATH}/../apps/ -type d -name samplemain | xargs rm -r
 
 # Delete info.xml files for the apps, located in /scripts/apps/
 find ${PARENT_PATH}/../apps/ -name info.xml | xargs rm
