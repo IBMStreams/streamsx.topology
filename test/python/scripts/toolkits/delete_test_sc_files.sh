8f919bfc68a60d4e4802560013a2883d4920549d
Karsten12 <kfonseca@berkeley.edu>
2019-10-24 09:25:01 -0700

    updated delete script, all test pass

--- test/python/scripts/toolkits/delete_test_sc_files.sh
+++ test/python/scripts/toolkits/delete_test_sc_files.sh
@@ -6,17 +6,17 @@
 # Path of this file /test/python/scripts/toolkits
 PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
-# Delete info.xml files for the toolkits located in /scripts/toolkits/
+# Delete generated info.xml files for the toolkits located in /scripts/toolkits/
 find ${PARENT_PATH} -name info.xml | xargs rm
 
 # Delete generated com.example.*.test_tk_*.test1 folder
 find $PARENT_PATH -type d -name 'com.example.*.test_tk_*.test1' | xargs rm -r
 
-# Delete toolkit.xml files for the toolkits located in /scripts/toolkits/
+# Delete generated toolkit.xml files for the toolkits located in /scripts/toolkits/
 find ${PARENT_PATH} -name toolkit.xml | xargs rm
 
-# Delete generated samplemain folder
+# Delete generated samplemain folder, located in /scripts/apps/
 find ${PARENT_PATH}/../apps/ -type d -name samplemain | xargs rm -r
 
-# Delete info.xml files for the apps, located in /scripts/apps/
+# Delete generated info.xml files for the apps, located in /scripts/apps/
 find ${PARENT_PATH}/../apps/ -name info.xml | xargs rm
