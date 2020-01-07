fae32d67d6115190a5ec9db405a4b91efafbfd2d
Dan Debrunner <djd@debrunners.com>
2020-01-07 00:39:12 +0000

    Switch back to 1.x for next release

--- com.ibm.streamsx.topology/opt/python/packages/streamsx/_streams/_version.py
+++ com.ibm.streamsx.topology/opt/python/packages/streamsx/_streams/_version.py
@@ -3,7 +3,7 @@
 # Copyright IBM Corp. 2018,2019
 
 ## Common PEP396 version for modules shipped with streamsx packages
-__version__='2.0.0a'
+__version__='1.14.0a'
 
 import pkg_resources
 import sys
