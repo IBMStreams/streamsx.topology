54d6e06836b56c1986469f8c08247a81d9db351e
markheger <mark-oliver.heger@de.ibm.com>
2020-03-19 12:03:08 +0000

    add -f option

--- test/python/scripts/sc_test_files/toolkits/delete_test_sc_files.sh
+++ test/python/scripts/sc_test_files/toolkits/delete_test_sc_files.sh
@@ -7,16 +7,16 @@
 PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 # Delete generated info.xml files for the toolkits located in /scripts/sc_test_files/toolkits/
-find ${PARENT_PATH} -name info.xml | xargs rm
+find ${PARENT_PATH} -name info.xml | xargs rm -f
 
 # Delete generated com.example.*.test_tk_*.test1 folder
-find $PARENT_PATH -type d -name 'com.example.*.test_tk_*.test1' | xargs rm -r
+find $PARENT_PATH -type d -name 'com.example.*.test_tk_*.test1' | xargs rm -rf
 
 # Delete generated toolkit.xml files for the toolkits located in /scripts/sc_test_files/toolkits/
-find ${PARENT_PATH} -name toolkit.xml | xargs rm
+find ${PARENT_PATH} -name toolkit.xml | xargs rm -f
 
 # Delete generated samplemain folder, located in /scripts/sc_test_files/apps/
-find ${PARENT_PATH}/../apps/ -type d -name samplemain | xargs rm -r
+find ${PARENT_PATH}/../apps/ -type d -name samplemain | xargs rm -rf
 
 # Delete generated info.xml files for the apps, located in /scripts/sc_test_files/apps/
-find ${PARENT_PATH}/../apps/ -name info.xml | xargs rm
+find ${PARENT_PATH}/../apps/ -name info.xml | xargs rm -f
