ac1e317bcde0a6a7749b2a9784fa1483048e6687
markheger <mark-oliver.heger@de.ibm.com>
2020-04-02 11:32:30 +0000

    add test cases, #756

--- test/python/topology/test2_pkg.py
+++ test/python/topology/test2_pkg.py
@@ -4,6 +4,7 @@ import unittest
 import os
 import sys
 import itertools
+from pathlib import Path
 
 import test_functions
 from test_utilities import standalone
@@ -17,8 +18,34 @@ from streamsx.topology.context import ConfigParams
 import test_package.test_subpackage.test_module
 import test2_pkg_helpers
 
+
+
 class TestPackages(unittest.TestCase):
   _multiprocess_can_split_ = True
+
+  @classmethod
+  def setUpClass(cls):
+      my_path = Path(__file__).parent
+      # test_symlink_to_module -> test_symlink_to_module_real
+      dst = os.path.join(my_path, 'test_symlink_to_module')
+      src = os.path.join(my_path, 'test_symlink_to_module_real')
+      if not Path(dst).is_symlink():
+          os.symlink(src, dst)
+      # symlinkmodule.py -> test_symlink_to_module_real/customsource.py
+      dst = os.path.join(my_path, 'symlinkmodule.py')
+      src = os.path.join(my_path, 'test_symlink_to_module_real', 'customsource.py')
+      if not Path(dst).is_symlink():
+          os.symlink(src, dst)
+
+  @classmethod
+  def tearDownClass(cls):
+      my_path = Path(__file__).parent
+      dst = os.path.join(my_path, 'test_symlink_to_module')
+      if Path(dst).is_symlink():
+          os.unlink(dst)
+      dst = os.path.join(my_path, 'symlinkmodule.py')
+      if Path(dst).is_symlink():
+          os.unlink(dst)
 
   def setUp(self):
       Tester.setup_standalone(self)
@@ -65,12 +92,35 @@ class TestPackages(unittest.TestCase):
           if pypath is not None:
               os.environ['PYTHONPATH'] = pypath
 
+  # directory symlink to module
+  def test_TopologySymlinkDir(self):
+      # test_symlink_to_module -> test_symlink_to_module_real
+      from test_symlink_to_module.customsource import customsource
+      topo = Topology("test_TopologySymlinkDir")
+      st = topo.source(customsource())
+
+      tester = Tester(topo)
+      tester.tuple_count(st, 2, exact=True)
+      tester.test(self.test_ctxtype, self.test_config)
+
+  # file symlink to module
+  def test_TopologySymlinkFile(self):
+      # symlinkmodule.py -> test_symlink_to_module_real/customsource.py
+      from symlinkmodule import customsource
+      topo = Topology("test_TopologySymlinkFile")
+      st = topo.source(customsource())
+
+      tester = Tester(topo)
+      tester.tuple_count(st, 2, exact=True)
+      tester.test(self.test_ctxtype, self.test_config)
+
 class TestDistributedPackages(TestPackages):
 
     def setUp(self):
         Tester.setup_distributed(self)
         self.test_config[ConfigParams.SSL_VERIFY] = False
 
+    @unittest.skipIf('CP4D_URL' not in os.environ and 'STREAMS_REST_URL' not in os.environ, 'CP4D_URL/STREAMS_REST_URL not set')
     def test_add_pip_package(self):
         topo = Topology()
         topo.add_pip_package('pint')
@@ -80,6 +130,7 @@ class TestDistributedPackages(TestPackages):
         tester.contents(s, ['pint'])
         tester.test(self.test_ctxtype, self.test_config)
 
+    @unittest.skipIf('CP4D_URL' not in os.environ and 'STREAMS_REST_URL' not in os.environ, 'CP4D_URL/STREAMS_REST_URL not set')
     def test_add_pip_package_whl_from_url(self):
         topo = Topology()
         topo.add_pip_package('https://github.com/IBMStreams/streamsx.topology/raw/master/test/python/topology/test_package_whl/whl/tstexamplepkg-1.0-py3-none-any.whl', name='tstexamplepkg')
