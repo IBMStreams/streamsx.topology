a2c172e5350a758736dd68d73b01d030e7e2ce43
markheger <mark-oliver.heger@de.ibm.com>
2020-05-12 07:33:29 +0000

    getLogMessages() added

--- java/src/com/ibm/streamsx/rest/build/Build.java
+++ java/src/com/ibm/streamsx/rest/build/Build.java
@@ -7,6 +7,7 @@ package com.ibm.streamsx.rest.build;
 import java.io.File;
 import java.io.IOException;
 import java.util.List;
+import java.util.ArrayList;
 
 import org.apache.http.client.fluent.Request;
 import org.apache.http.entity.ContentType;
@@ -173,15 +174,23 @@ public class Build extends Element {
 		} while ("building".equals(getStatus()) || "waiting".equals(getStatus()) || "submitted".equals(getStatus()));
 		
 		StreamsRestUtils.TRACE.severe("The submitted archive " + archive.getName() + " failed to build with status " + getStatus() + ".");
-		
+		getLogMessages();
+    	
+    	return this;
+    }
+    
+    public List<String> getLogMessages() throws IOException, InterruptedException {
+    	List<String> result = new ArrayList<>();
+    	
 		Request gr = Request.Get(this.logMessages).addHeader("Authorization", connection().getAuthorization());
 				
 		String output = StreamsRestUtils.requestTextResponse(connection().executor, gr);
 		String[] lines = output.split("\\R");
-		for (String line : lines)
+		for (String line : lines) {
 		    StreamsRestUtils.TRACE.severe(line);
-    	
-    	return this;
+		    result.add(line);
+		}
+		return result;
     }
     
     public Build submit(String propertyName, JsonObject property) throws IOException {
