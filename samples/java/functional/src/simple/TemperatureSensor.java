ecf626ab80db1f472dcf21294e47c0dcde606597
markheger <mark-oliver.heger@de.ibm.com>
2020-07-06 07:57:12 +0000

    add sample TemperaturSensor.java

---
+++ samples/java/functional/src/simple/TemperatureSensor.java
@@ -0,0 +1,131 @@
+/*
+# Licensed Materials - Property of IBM
+# Copyright IBM Corp. 2020  
+ */
+package simple;
+
+import java.util.Arrays;
+import java.util.Random;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import com.ibm.streamsx.topology.TStream;
+import com.ibm.streamsx.topology.Topology;
+import com.ibm.streamsx.topology.context.StreamsContextFactory;
+import com.ibm.streamsx.topology.function.Function;
+import com.ibm.streamsx.topology.function.Supplier;
+import com.ibm.streamsx.topology.TWindow;
+
+/**
+ * Sample Hello World topology application. This Java application builds a
+ * simple topology that prints Hello World to standard output. <BR>
+ * The application implements the typical pattern of code that declares a
+ * topology followed by submission of the topology to a Streams context
+ * {@code com.ibm.streamsx.topology.context.StreamsContext}.
+ * <BR>
+ * This demonstrates the mechanics of declaring a topology and executing it.
+ * <P>
+ * This may be executed from the {@code samples/java/functional} directory as:
+ * <UL>
+ * <LI>{@code ant run.helloworld} - Using Apache Ant, this will run in embedded
+ * mode.</LI>
+ * <LI>
+ * {@code java -jar funcsamples.jar:../com.ibm.streamsx.topology/lib/com.ibm.streamsx.topology.jar:$STREAMS_INSTALL/lib/com.ibm.streams.operator.samples.jar
+ *  simple.HelloWorld [CONTEXT_TYPE] 
+ * } - Run directly from the command line.
+ * </LI>
+ * If no arguments are provided then the topology is executed in embedded mode,
+ * within this JVM.
+ * <BR>
+ * <i>CONTEXT_TYPE</i> is one of:
+ * <UL>
+ * <LI>{@code DISTRIBUTED} - Run as an IBM Streams distributed
+ * application.</LI>
+ * <LI>{@code STANDALONE} - Run as an IBM Streams standalone
+ * application.</LI>
+ * <LI>{@code EMBEDDED} - Run embedded within this JVM.</LI>
+ * <LI>{@code BUNDLE} - Create an IBM Streams application bundle.</LI>
+ * <LI>{@code TOOLKIT} - Create an IBM Streams application toolkit.</LI>
+ * </UL>
+ * </LI>
+ * <LI>
+ * An application execution within your IDE once you set the class path to include the correct jars.</LI>
+ * </UL>
+ * </P>
+ */
+public class TemperatureSensor {
+
+    /**
+     * Sample Hello World topology application.
+     * This Java application builds a simple topology
+     * that prints Hello World to standard output.
+     * <BR>
+     * The application implements the typical pattern
+     * of code that declares a topology followed by
+     * submission of the topology to a Streams context
+     * (@code com.ibm.streamsx.topology.context.StreamsContext}.
+
+     * @param args
+     * @throws Exception
+     */
+    public static void main(String[] args) throws Exception {
+
+        /*
+         * Create the container for the topology that will
+         * hold the streams of tuples.
+         */
+        Topology topology = new Topology("TemperatureSensor");
+
+        Random random = new Random();
+
+        @SuppressWarnings("unchecked")
+        TStream<Double> readingsSource = topology.endlessSource(new Supplier<Double>(){
+            @Override
+            public Double get() {
+                return random.nextGaussian();
+            }
+
+        });
+
+        Supplier<Integer> width = topology.createSubmissionParameter("width", Integer.class);
+        TStream<Double> parallelReadings = readingsSource.parallel(width);
+
+        TStream<Double> parallelRegion = parallelReadings.transform(new Function<Double, Double>(){
+            @Override
+            public Double apply(Double temp) {
+                return temp;
+            }
+        });
+
+        TStream<Double> readings = parallelRegion.endParallel();
+
+
+        Supplier<Long> time = topology.createSubmissionParameter("time", Long.class);
+        TWindow<Double, ?> lastNSeconds = readings.last(time, TimeUnit.SECONDS);
+        
+        TStream<Double> maxTemp = lastNSeconds.aggregate(new Function<List<Double>, Double>(){
+            @Override
+            public Double apply(List<Double> temps) {
+                Double max = temps.get(0);
+                for(Double temp : temps){
+                    if(temp > max)
+                        max = temp;
+                }
+                return max;
+            }
+
+        });
+        maxTemp.print();
+        /*
+         * Now execute the topology by submitting to a StreamsContext.
+         * If no argument is provided then the topology is executed
+         * within this JVM (StreamsContext.Type.EMBEDDED).
+         * Otherwise the first and only argument is taken as the
+         * String representation of the 
+         */
+        if (args.length == 0)
+            StreamsContextFactory.getEmbedded().submit(topology).get();
+        else
+            StreamsContextFactory.getStreamsContext(args[0]).submit(topology)
+                    .get();
+    }
+}
