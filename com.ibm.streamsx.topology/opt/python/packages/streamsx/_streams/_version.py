cbc8af279dd4917cdd890c264298231094b91946
RolefH <rolef.heinrich@de.ibm.com>
2020-10-27 11:50:28 +0100

    version bump to 1.17.0

--- com.ibm.streamsx.topology/opt/python/packages/streamsx/_streams/_version.py
+++ com.ibm.streamsx.topology/opt/python/packages/streamsx/_streams/_version.py
@@ -3,7 +3,7 @@
 # Copyright IBM Corp. 2018,2019
 
 ## Common PEP396 version for modules shipped with streamsx packages
-__version__='1.17.0a'
+__version__='1.17.0'
 
 import pkg_resources
 import sys
